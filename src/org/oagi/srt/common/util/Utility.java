50a72e5f796ba90382eb85269c1ae5d2b5ec26aa
yslee <gazuo75@gmail.com>
2014-08-15 13:17:34 -0400

    move DBSetup method to utility class

--- src/org/oagi/srt/common/util/Utility.java
+++ src/org/oagi/srt/common/util/Utility.java
@@ -1,9 +1,56 @@
 package org.oagi.srt.common.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
 import java.util.UUID;
+
+import org.chanchan.common.persistence.db.ConnectionPoolManager;
+import org.chanchan.common.util.ServerProperties;
+import org.oagi.srt.common.SRTConstants;
+import org.oagi.srt.startup.SRTInitializer;
+import org.oagi.srt.startup.SRTInitializerException;
 
 public class Utility {
 	public static String generateGUID(){
 		return "oagis-id-" + UUID.randomUUID().toString().replaceAll("-", "");
+	}
+	public static String filepath() {
+		String filepath = "C:\\Users\\jnl18\\Documents\\OAG\\Jaehun\\";
+		return filepath;
+	}
+	public static void dbSetup() throws SRTInitializerException {
+		ServerProperties props = ServerProperties.getInstance();
+		String _propFile = "/" + SRTConstants.SRT_PROPERTIES_FILE_NAME;
+		try {
+			InputStream is = SRTInitializer.class.getResourceAsStream(_propFile);
+			if (is == null) {
+				throw new SRTInitializerException(_propFile + " not found!");
+			}
+			try {
+				props.load(is, true);
+			} catch (IOException e) {
+				throw new SRTInitializerException(_propFile + " cannot be read...");
+			}
+		} catch (Exception e) {
+			System.out.println("[SRTInitializer] Fail to Getting "
+					+ SRTConstants.SRT_PROPERTIES_FILE_NAME + " URL : "
+					+ e.toString());
+		}
+		try {
+			ConnectionPoolManager cpm = ConnectionPoolManager.getInstance();
+			String poolName = cpm.getDefaultPoolName();
+			System.out.println("DefaultPoolName:" + poolName);
+			Connection dbConnection = cpm.getConnection(poolName);
+			dbConnection.close();
+			System.out.println("DB Connection Pool initialized...");
+			cpm.release();
+		} catch (Exception e) {
+			System.out.println("[SRTInitializer] Fail to Creating Connection Pool : "
+					+ e.toString());
+			e.printStackTrace();
+			throw new SRTInitializerException("[SRTInitializer] Fail to Creating Connection Pool : "
+					+ e.toString());
+		}
 	}
 }
