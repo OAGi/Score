a80e284dacd4db125d5a12679ded22c26e7121ca
Hakju Oh <evanescent85@gmail.com>
2016-09-08 16:58:15 -0400

    Use SequenceGenerator for Identifier Generation

--- srt-repository/src/main/java/org/oagi/srt/repository/entity/CoreDataTypeAllowedPrimitive.java
+++ srt-repository/src/main/java/org/oagi/srt/repository/entity/CoreDataTypeAllowedPrimitive.java
@@ -1,7 +1,5 @@
 package org.oagi.srt.repository.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -13,16 +11,7 @@ public class CoreDataTypeAllowedPrimitive implements Serializable {
 
     @Id
     @GeneratedValue(generator = SEQUENCE_NAME, strategy = GenerationType.SEQUENCE)
-    @GenericGenerator(
-            name = SEQUENCE_NAME,
-            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-            parameters = {
-                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = SEQUENCE_NAME),
-                    @org.hibernate.annotations.Parameter(name = "optimizer", value = "pooled-lo"),
-                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1"),
-            }
-    )
+    @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME)
     private long cdtAwdPriId;
 
     @Column(nullable = false)
