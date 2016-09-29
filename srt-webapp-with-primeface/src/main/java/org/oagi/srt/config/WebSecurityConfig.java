90e9bc4322e767df52cb9fe9126c6073c1d72a42
Hakju Oh <evanescent85@gmail.com>
2016-09-29 14:57:45 -0400

    Apply login page

---
+++ srt-webapp-with-primeface/src/main/java/org/oagi/srt/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package org.oagi.srt.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("userDetailsService")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl(
+                        "/login.xhtml");
+
+        http.authorizeRequests()
+                .antMatchers("/", "/index.xhtml").permitAll()
+                .antMatchers("/javax.faces.resource/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .loginPage("/login.xhtml").permitAll()
+                .and()
+                    .logout().permitAll();
+    }
+
+}
