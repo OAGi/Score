7c8ad78819f9722b58ed68d5ac9ae7d7f86732dc
Hakju Oh <evanescent85@gmail.com>
2016-10-04 18:06:28 -0400

    Extract 'srt.js'

---
+++ srt-webapp-with-primeface/src/main/resources/META-INF/resources/js/srt.js
@@ -0,0 +1,7 @@
+function initDataTablePaginator() {
+    console.log('aa');
+    $('.ui-icon-seek-first').text('First');
+    $('.ui-icon-seek-prev').text('Previous');
+    $('.ui-icon-seek-next').text('Next');
+    $('.ui-icon-seek-end').text('Last');
+}
