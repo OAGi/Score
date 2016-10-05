25ae86ab09ea6ad8e75029bf6abde2ebee2ec0aa
Hakju Oh <evanescent85@gmail.com>
2016-10-04 20:36:24 -0400

    If dataTable has no data, paginator doesn't show up

--- srt-webapp-with-primeface/src/main/java/org/oagi/srt/web/jsf/component/SRTDataTableRenderer.java
+++ srt-webapp-with-primeface/src/main/java/org/oagi/srt/web/jsf/component/SRTDataTableRenderer.java
@@ -1,38 +1,15 @@
 package org.oagi.srt.web.jsf.component;
 
-import org.primefaces.component.api.DynamicColumn;
-import org.primefaces.component.api.UIColumn;
 import org.primefaces.component.api.UIData;
-import org.primefaces.component.column.Column;
-import org.primefaces.component.columngroup.ColumnGroup;
-import org.primefaces.component.columns.Columns;
 import org.primefaces.component.datatable.DataTable;
 import org.primefaces.component.datatable.DataTableRenderer;
-import org.primefaces.component.datatable.feature.DataTableFeature;
-import org.primefaces.component.datatable.feature.DataTableFeatureKey;
-import org.primefaces.component.datatable.feature.RowExpandFeature;
-import org.primefaces.component.datatable.feature.SortFeature;
 import org.primefaces.component.paginator.PaginatorElementRenderer;
-import org.primefaces.component.row.Row;
-import org.primefaces.component.subtable.SubTable;
-import org.primefaces.component.summaryrow.SummaryRow;
-import org.primefaces.model.SortMeta;
-import org.primefaces.model.SortOrder;
-import org.primefaces.util.*;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.WidgetBuilder;
 
-import javax.el.ELContext;
-import javax.el.MethodExpression;
-import javax.el.ValueExpression;
-import javax.faces.FacesException;
-import javax.faces.component.UIComponent;
-import javax.faces.component.UINamingContainer;
-import javax.faces.component.UIPanel;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
-import javax.faces.model.SelectItem;
 import java.io.IOException;
-import java.util.*;
-import java.util.logging.Logger;
 
 public class SRTDataTableRenderer extends DataTableRenderer {
 
@@ -44,6 +21,70 @@ public class SRTDataTableRenderer extends DataTableRenderer {
 
     public static PaginatorElementRenderer removePaginatorElement(String element) {
         return DataRenderer.removePaginatorElement(element);
+    }
+
+    protected void encodeMarkup(FacesContext context, DataTable table) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = table.getClientId(context);
+        boolean scrollable = table.isScrollable();
+        boolean hasPaginator = table.isPaginator();
+        String style = table.getStyle();
+        String paginatorPosition = table.getPaginatorPosition();
+        int frozenColumns = table.getFrozenColumns();
+        boolean hasFrozenColumns = (frozenColumns != 0);
+        boolean hasData = table.getRowCount() > 0;
+
+        //style class
+        String containerClass = scrollable ? DataTable.CONTAINER_CLASS + " " + DataTable.SCROLLABLE_CONTAINER_CLASS : DataTable.CONTAINER_CLASS;
+        containerClass = table.getStyleClass() != null ? containerClass + " " + table.getStyleClass() : containerClass;
+        if (table.isResizableColumns()) containerClass = containerClass + " " + DataTable.RESIZABLE_CONTAINER_CLASS;
+        if (table.isStickyHeader()) containerClass = containerClass + " " + DataTable.STICKY_HEADER_CLASS;
+        if (ComponentUtils.isRTL(context, table)) containerClass = containerClass + " " + DataTable.RTL_CLASS;
+        if (table.isReflow()) containerClass = containerClass + " " + DataTable.REFLOW_CLASS;
+        if (hasFrozenColumns) containerClass = containerClass + " ui-datatable-frozencolumn";
+
+        writer.startElement("div", table);
+        writer.writeAttribute("id", clientId, "id");
+        writer.writeAttribute("class", containerClass, "styleClass");
+        if (style != null) {
+            writer.writeAttribute("style", style, "style");
+        }
+
+        if (table.isReflow()) {
+            encodeSortableHeaderOnReflow(context, table);
+        }
+
+        encodeFacet(context, table, table.getHeader(), DataTable.HEADER_CLASS);
+
+        if (hasPaginator && !paginatorPosition.equalsIgnoreCase("bottom") && hasData) {
+            encodePaginatorMarkup(context, table, "top");
+        }
+
+        if (scrollable) {
+            encodeScrollableTable(context, table);
+        } else {
+            encodeRegularTable(context, table);
+        }
+
+        if (hasPaginator && !paginatorPosition.equalsIgnoreCase("top") && hasData) {
+            encodePaginatorMarkup(context, table, "bottom");
+        }
+
+        encodeFacet(context, table, table.getFooter(), DataTable.FOOTER_CLASS);
+
+        if (table.isSelectionEnabled()) {
+            encodeStateHolder(context, table, table.getClientId(context) + "_selection", table.getSelectedRowKeysAsString());
+        }
+
+        if (table.isDraggableColumns()) {
+            encodeStateHolder(context, table, table.getClientId(context) + "_columnOrder", null);
+        }
+
+        if (scrollable) {
+            encodeStateHolder(context, table, table.getClientId(context) + "_scrollState", table.getScrollState());
+        }
+
+        writer.endElement("div");
     }
 
     @Override
