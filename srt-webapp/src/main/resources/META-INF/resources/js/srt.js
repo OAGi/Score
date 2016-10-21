622bf300cda9586743caf2d67a4cd51d50315627
Hakju Oh <evanescent85@gmail.com>
2016-10-21 04:11:05 -0400

    Add 403 error handling code

--- srt-webapp/src/main/resources/META-INF/resources/js/srt.js
+++ srt-webapp/src/main/resources/META-INF/resources/js/srt.js
@@ -1,7 +1,21 @@
-function initDataTablePaginator() {
-    console.log('aa');
-    $('.ui-icon-seek-first').text('First');
-    $('.ui-icon-seek-prev').text('Previous');
-    $('.ui-icon-seek-next').text('Next');
-    $('.ui-icon-seek-end').text('Last');
+var adjustMainHeight = function () {
+    var headerHeight = $('.header').outerHeight();
+    var footerHeight = $('.footer').parent().parent().parent().outerHeight();
+    var height = $(this).height();
+
+    var mainHeight = height - headerHeight - footerHeight;
+    $('.main').css('min-height', mainHeight + "px");
+};
+adjustMainHeight();
+$(window).resize(adjustMainHeight);
+
+var jsf = jsf || {};
+if (jsf.ajax) {
+    jsf.ajax.addOnError(function (data) {
+        console.log(data);
+        var response = $.parseJSON(data.responseText);
+        if (response.status == 403) {
+            location.href = '/';
+        }
+    });
 }
