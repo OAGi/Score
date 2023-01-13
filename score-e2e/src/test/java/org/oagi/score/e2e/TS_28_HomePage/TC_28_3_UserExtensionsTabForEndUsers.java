f82f753200271eb3bda0560f233f8cc6cd34bcbf
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-01-13 10:46:50 -0500

    add test TC28_6 & 7

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_28_HomePage/TC_28_3_UserExtensionsTabForEndUsers.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_28_HomePage/TC_28_3_UserExtensionsTabForEndUsers.java
@@ -388,6 +388,59 @@ public class TC_28_3_UserExtensionsTabForEndUsers extends BaseTest {
         click(homePage.getUserExtensionsTab());
 
         HomePage.UEsByUsersAndStatesPanel uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
+        assertTrue(uesByUsersAndStatesPanel.getTableRecordByValue(endUser1.getLoginId()).isDisplayed());
+        assertTrue(uesByUsersAndStatesPanel.getTableRecordByValue(endUser2.getLoginId()).isDisplayed());
+
+        WebElement tr = uesByUsersAndStatesPanel.getTableRecordByValue(endUser1.getLoginId());
+        WebElement td_WIP = uesByUsersAndStatesPanel.getColumnByName(tr, "WIP");
+        WebElement td_QA = uesByUsersAndStatesPanel.getColumnByName(tr, "QA");
+        WebElement td_Production = uesByUsersAndStatesPanel.getColumnByName(tr, "Production");
+        WebElement td_Total = uesByUsersAndStatesPanel.getColumnByName(tr, "total");
+
+        assertEquals(container1.numberOfWIPUEGs, Integer.valueOf(getText(td_WIP)));
+        assertEquals(container1.numberOfQAUEGs, Integer.valueOf(getText(td_QA)));
+        assertEquals(container1.numberOfProductionUEGs, Integer.valueOf(getText(td_Production)));
+        assertEquals(container1.numberOfWIPUEGs +
+                        container1.numberOfQAUEGs +
+                        container1.numberOfProductionUEGs,
+                Integer.valueOf(getText(td_Total)));
+
+        WebElement tr2 = uesByUsersAndStatesPanel.getTableRecordByValue(endUser2.getLoginId());
+        WebElement td2_WIP = uesByUsersAndStatesPanel.getColumnByName(tr2, "WIP");
+        WebElement td2_QA = uesByUsersAndStatesPanel.getColumnByName(tr2, "QA");
+        WebElement td2_Production = uesByUsersAndStatesPanel.getColumnByName(tr2, "Production");
+        WebElement td2_Total = uesByUsersAndStatesPanel.getColumnByName(tr2, "total");
+
+        assertEquals(container2.numberOfWIPUEGs, Integer.valueOf(getText(td2_WIP)));
+        assertEquals(container2.numberOfQAUEGs, Integer.valueOf(getText(td2_QA)));
+        assertEquals(container2.numberOfProductionUEGs, Integer.valueOf(getText(td2_Production)));
+        assertEquals(container2.numberOfWIPUEGs +
+                        container2.numberOfQAUEGs +
+                        container2.numberOfProductionUEGs,
+                Integer.valueOf(getText(td2_Total)));
+
+    }
+
+    @Test
+    @DisplayName("TC_28_3_6")
+    public void end_user_can_select_user_to_narrow_down_list_and_see_only_his_extensions_per_state_in_user_extensions_by_users_and_states_panel() {
+        AppUserObject endUser1 = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser1);
+        AppUserObject endUser2 = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser2);
+
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("10.8.4");
+
+        NamespaceObject endUserNamespace = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUser1);
+
+        UserTopLevelASBIEPContainer container1 = new UserTopLevelASBIEPContainer(endUser1, release, endUserNamespace);
+        UserTopLevelASBIEPContainer container2 = new UserTopLevelASBIEPContainer(endUser2, release, endUserNamespace);
+
+        HomePage homePage = loginPage().signIn(endUser1.getLoginId(), endUser1.getPassword());
+        homePage.setBranch(release.getReleaseNumber());
+        click(homePage.getUserExtensionsTab());
+
+        HomePage.UEsByUsersAndStatesPanel uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
         uesByUsersAndStatesPanel.setUsername(endUser1.getLoginId());
         assertTrue(uesByUsersAndStatesPanel.getTableRecordByValue(endUser1.getLoginId()).isDisplayed());
 
@@ -421,17 +474,76 @@ public class TC_28_3_UserExtensionsTabForEndUsers extends BaseTest {
                         container2.numberOfProductionUEGs,
                 Integer.valueOf(getText(td2_Total)));
 
-    }
-
-    @Test
-    @DisplayName("TC_28_3_6")
-    public void end_user_can_select_user_to_narrow_down_list_and_see_only_his_extensions_per_state_in_user_extensions_by_users_and_states_panel() {
 
     }
 
     @Test
     @DisplayName("TC_28_3_7")
     public void end_user_can_click_table_cell_view_relevant_user_extensions_in_user_extensions_by_users_and_states_panel() {
+
+        AppUserObject endUser1 = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser1);
+
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("10.8.4");
+
+        NamespaceObject endUserNamespace = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUser1);
+
+        UserTopLevelASBIEPContainer container1 = new UserTopLevelASBIEPContainer(endUser1, release, endUserNamespace);
+
+        HomePage homePage = loginPage().signIn(endUser1.getLoginId(), endUser1.getPassword());
+        homePage.setBranch(release.getReleaseNumber());
+
+        HomePage.UEsByUsersAndStatesPanel uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
+
+        click(homePage.getScoreLogo()); // to go to the home page again.
+        click(homePage.getUserExtensionsTab());
+        uesByUsersAndStatesPanel.setUsername(endUser1.getLoginId()); //select endUser1
+
+        WebElement tr = uesByUsersAndStatesPanel.getTableRecordByValue(endUser1.getLoginId());
+        WebElement td_Total = uesByUsersAndStatesPanel.getColumnByName(tr, "total");
+        assertEquals(container1.numberOfWIPUEGs +
+                        container1.numberOfQAUEGs +
+                        container1.numberOfProductionUEGs,
+                Integer.valueOf(getText(td_Total)));
+
+        ViewEditCoreComponentPage viewEditCCPageForWIP = uesByUsersAndStatesPanel.openViewEditCCPageByUsernameAndColumnName(
+                endUser1.getLoginId(), "WIP");
+        assertEquals(container1.numberOfWIPUEGs, viewEditCCPageForWIP.getNumberOfOnlyCCsPerStateAreListed("WIP"));
+        assertEquals(0, viewEditCCPageForWIP.getNumberOfOnlyCCsPerStateAreListed("QA"));
+        assertEquals(0, viewEditCCPageForWIP.getNumberOfOnlyCCsPerStateAreListed("Production"));
+
+        click(homePage.getScoreLogo()); // to go to the home page again.
+        click(homePage.getUserExtensionsTab());
+        uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
+        uesByUsersAndStatesPanel.setUsername(endUser1.getLoginId()); //select endUser1
+        ViewEditCoreComponentPage viewEditCCPageForQA = uesByUsersAndStatesPanel.openViewEditCCPageByUsernameAndColumnName(
+                endUser1.getLoginId(), "QA");
+        assertEquals(0, viewEditCCPageForQA.getNumberOfOnlyCCsPerStateAreListed("WIP"));
+        assertEquals(container1.numberOfQAUEGs, viewEditCCPageForQA.getNumberOfOnlyCCsPerStateAreListed("QA"));
+        assertEquals(0, viewEditCCPageForQA.getNumberOfOnlyCCsPerStateAreListed("Production"));
+
+        click(homePage.getScoreLogo()); // to go to the home page again.
+        click(homePage.getUserExtensionsTab());
+        uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
+        uesByUsersAndStatesPanel.setUsername(endUser1.getLoginId()); //select endUser1
+        ViewEditCoreComponentPage viewEditCCPageForProduction = uesByUsersAndStatesPanel.openViewEditCCPageByUsernameAndColumnName(
+                endUser1.getLoginId(), "Production");
+        assertEquals(0, viewEditCCPageForProduction.getNumberOfOnlyCCsPerStateAreListed("WIP"));
+        assertEquals(0, viewEditCCPageForProduction.getNumberOfOnlyCCsPerStateAreListed("QA"));
+        assertEquals(container1.numberOfProductionUEGs, viewEditCCPageForProduction.getNumberOfOnlyCCsPerStateAreListed("Production"));
+
+        click(homePage.getScoreLogo()); // to go to the home page again.
+        click(homePage.getUserExtensionsTab());
+        uesByUsersAndStatesPanel = homePage.openUEsByUsersAndStatesPanel();
+        uesByUsersAndStatesPanel.setUsername(endUser1.getLoginId()); //select endUser1
+        ViewEditCoreComponentPage viewEditCCPageForTotal = uesByUsersAndStatesPanel.openViewEditCCPageByUsernameAndColumnName(
+                endUser1.getLoginId(), "total");
+        // The total number of randomly-generated CCs could be more than the default size of items, 10.
+        // Thus, it should set 'Items per page' to more than 10 to count the total number of CCs.
+        viewEditCCPageForTotal.setItemsPerPage(50);
+        assertEquals(container1.numberOfWIPUEGs, viewEditCCPageForTotal.getNumberOfOnlyCCsPerStateAreListed("WIP"));
+        assertEquals(container1.numberOfQAUEGs, viewEditCCPageForTotal.getNumberOfOnlyCCsPerStateAreListed("QA"));
+        assertEquals(container1.numberOfProductionUEGs, viewEditCCPageForTotal.getNumberOfOnlyCCsPerStateAreListed("Production"));
 
     }
 
