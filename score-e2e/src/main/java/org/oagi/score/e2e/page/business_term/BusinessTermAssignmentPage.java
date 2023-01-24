3d6513401dec1144ec2e7aabb8c9edf006ade19a
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-01-24 15:20:51 -0500

    Add BusinessTerm package

---
+++ score-e2e/src/main/java/org/oagi/score/e2e/page/business_term/BusinessTermAssignmentPage.java
@@ -0,0 +1,8 @@
+package org.oagi.score.e2e.page.business_term;
+
+import org.oagi.score.e2e.page.Page;
+/**
+ * An interface for 'Business Term Assignment' page
+ */
+public interface BusinessTermAssignmentPage extends Page {
+}
---
+++ score-e2e/src/main/java/org/oagi/score/e2e/page/business_term/ViewEditBusinessTermPage.java
@@ -0,0 +1,9 @@
+package org.oagi.score.e2e.page.business_term;
+
+import org.oagi.score.e2e.page.Page;
+
+/**
+ * An interface for 'View/Edit Business Term' page
+ */
+public interface ViewEditBusinessTermPage extends Page {
+}
