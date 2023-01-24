01239b9832f6faf4295c188c22a6941089add11f
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-01-24 17:02:25 -0500

    update BusinessTermObject

--- score-e2e/src/main/java/org/oagi/score/e2e/obj/BusinessTermObject.java
+++ score-e2e/src/main/java/org/oagi/score/e2e/obj/BusinessTermObject.java
@@ -2,20 +2,34 @@ package org.oagi.score.e2e.obj;
 
 import lombok.Data;
 
+import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 import static org.apache.commons.lang3.RandomStringUtils.*;
 
 @Data
 public class BusinessTermObject {
-    private String bieBiztermId;
-    private String bieId;
-    private String primaryIndicator;
-    private String typeCode;
+    private String businessTermId;
+
     private String businessTerm;
-    private String guid;
+
+    private String definition;
+
+    private String comment;
+
     private String externalReferenceUri;
+
+    private String externalReferenceId;
+
+    private String guid;
+
+    private LocalDateTime lastUpdateTimestamp;
+
+    private BigInteger createdBy;
+
+    private BigInteger lastUpdatedBy;
 
     public static BusinessTermObject createRandomBusinessTerm(AppUserObject creator) {
         return createRandomBusinessTerm(creator, "bt");
@@ -23,10 +37,15 @@ public class BusinessTermObject {
 
     public static BusinessTermObject createRandomBusinessTerm(AppUserObject creator, String namePrefix) {
         BusinessTermObject businessTerm = new BusinessTermObject();
+        businessTerm.setBusinessTermId(randomNumeric(8,15));
         businessTerm.setGuid(UUID.randomUUID().toString().replaceAll("-", ""));
         businessTerm.setBusinessTerm(namePrefix + "_" + randomAlphanumeric(5, 10));
-        businessTerm.setExternalReferenceUri("http://www." + randomAscii(3,8) + ".com" + businessTerm.getBieBiztermId());
-        businessTerm.setBieBiztermId(randomNumeric(1,10));
+        businessTerm.setExternalReferenceUri("http://www." + randomAscii(3,8) + ".com" + businessTerm.getExternalReferenceId());
+        businessTerm.setExternalReferenceId(randomNumeric(1,10));
+        businessTerm.setDefinition(randomPrint(50, 100).trim());
+        businessTerm.setComment(randomPrint(20,50).trim());
+        businessTerm.setCreatedBy(creator.getAppUserId());
+        businessTerm.setLastUpdatedBy(creator.getAppUserId());
         return businessTerm;
     }
 
