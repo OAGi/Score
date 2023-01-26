321d844ba7a9fa53bd42431c4b011fe7646095dc
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-01-26 11:26:04 -0500

    update ViewEditBusinessTermPage

--- score-e2e/src/main/java/org/oagi/score/e2e/impl/page/business_term/ViewEditBusinessTermPageImpl.java
+++ score-e2e/src/main/java/org/oagi/score/e2e/impl/page/business_term/ViewEditBusinessTermPageImpl.java
@@ -1,27 +1,56 @@
 package org.oagi.score.e2e.impl.page.business_term;
 
 import org.oagi.score.e2e.impl.page.BasePageImpl;
-import org.oagi.score.e2e.impl.page.context.CreateContextCategoryPageImpl;
-import org.oagi.score.e2e.impl.page.context.EditContextCategoryPageImpl;
-import org.oagi.score.e2e.obj.ContextCategoryObject;
+import org.oagi.score.e2e.obj.BusinessTermObject;
 import org.oagi.score.e2e.page.BasePage;
 import org.oagi.score.e2e.page.business_term.CreateBusinessTermPage;
 import org.oagi.score.e2e.page.business_term.EditBusinessTermPage;
 import org.oagi.score.e2e.page.business_term.ViewEditBusinessTermPage;
-import org.oagi.score.e2e.page.context.CreateContextCategoryPage;
-import org.oagi.score.e2e.page.context.EditContextCategoryPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.oagi.score.e2e.impl.PageHelper.*;
 
 public class ViewEditBusinessTermPageImpl extends BasePageImpl implements ViewEditBusinessTermPage {
 
+    private static final By UPDATER_SELECT_FIELD_LOCATOR =
+            By.xpath("//*[contains(text(), \"Updater\")]//ancestor::div[1]/mat-select[1]");
+
+    private static final By DROPDOWN_SEARCH_FIELD_LOCATOR =
+            By.xpath("//input[@aria-label=\"dropdown search\"]");
+
+    private static final By UPDATED_START_DATE_FIELD_LOCATOR =
+            By.xpath("//input[contains(@data-placeholder, \"Updated start date\")]");
+
+    private static final By UPDATED_END_DATE_FIELD_LOCATOR =
+            By.xpath("//input[contains(@data-placeholder, \"Updated end date\")]");
+
+    private static final By TERM_FIELD_LOCATOR =
+            By.xpath("//span[contains(text(), \"Term\")]//ancestor::div[1]/input");
+
+    private static final By EXTERNAL_REFERENCE_URI_FIELD_LOCATOR =
+            By.xpath("//span[contains(text(), \"External Reference URI\")]//ancestor::div[1]/input");
+
+    private static final By EXTERNAL_REFERENCE_ID_FIELD_LOCATOR =
+            By.xpath("//span[contains(text(), \"External Reference ID\")]//ancestor::div[1]/input");
+
+    private static final By SEARCH_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Search\")]//ancestor::button[1]");
     private static final By NEW_BUSINESS_TERM_BUTTON_LOCATOR =
             By.xpath("//span[contains(text(), \"New Business Term\")]//ancestor::button[1]");
-    public ViewEditBusinessTermPageImpl(BasePage parent){ super(parent);}
+
+    private static final By UPLOAD_BUSINESS_TERMS_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Upload Business Terms\")]//ancestor::button[1]");
+
+    private static final By DISCARD_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Discard\")]//ancestor::button[1]");
+
+
+    public ViewEditBusinessTermPageImpl(BasePage parent) {
+        super(parent);
+    }
 
     @Override
     protected String getPageUrl() {
@@ -41,6 +70,107 @@ public class ViewEditBusinessTermPageImpl extends BasePageImpl implements ViewEd
     }
 
     @Override
+    public WebElement getUpdaterSelectField() {
+        return visibilityOfElementLocated(getDriver(), UPDATER_SELECT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdater(String updater) {
+        click(getUpdaterSelectField());
+        sendKeys(visibilityOfElementLocated(getDriver(), DROPDOWN_SEARCH_FIELD_LOCATOR), updater);
+        WebElement searchedSelectField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[contains(text(), \"" + updater + "\")]"));
+        click(searchedSelectField);
+        escape(getDriver());
+    }
+
+    @Override
+    public WebElement getUpdatedStartDateField() {
+        return visibilityOfElementLocated(getDriver(), UPDATED_START_DATE_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdatedStartDate(LocalDateTime updatedStartDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        sendKeys(getUpdatedStartDateField(), formatter.format(updatedStartDate));
+    }
+
+    @Override
+    public WebElement getUpdatedEndDateField() {
+        return visibilityOfElementLocated(getDriver(), UPDATED_END_DATE_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdatedEndDate(LocalDateTime updatedEndDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        sendKeys(getUpdatedEndDateField(), formatter.format(updatedEndDate));
+    }
+
+    @Override
+    public WebElement getTermField() {
+        return visibilityOfElementLocated(getDriver(), TERM_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setTerm(String termName) {
+        sendKeys(getTermField(), termName);
+    }
+
+    @Override
+    public WebElement getExternalReferenceURIField() {
+        return visibilityOfElementLocated(getDriver(), EXTERNAL_REFERENCE_URI_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setExternalReferenceURI(String externalReferenceURI) {
+        sendKeys(getExternalReferenceURIField(), externalReferenceURI);
+    }
+
+    @Override
+    public WebElement getExternalReferenceIDField() {
+        return visibilityOfElementLocated(getDriver(), EXTERNAL_REFERENCE_ID_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setExternalReferenceID(String externalReferenceID) {
+        sendKeys(getExternalReferenceIDField(), externalReferenceID);
+    }
+
+    @Override
+    public WebElement getSearchButton() {
+        return elementToBeClickable(getDriver(), SEARCH_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public void hitSearchButton() {
+        retry(() -> click(getSearchButton()));
+    }
+
+    @Override
+    public WebElement getTableRecordAtIndex(int idx) {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//tbody/tr[" + idx + "]"));
+    }
+
+    @Override
+    public WebElement getColumnByName(WebElement tableRecord, String columnName) {
+        return tableRecord.findElement(By.className("mat-column-" + columnName));
+    }
+
+    @Override
+    public void goToNextPage() {
+        ((JavascriptExecutor) getDriver())
+                .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        click(elementToBeClickable(getDriver(), By.xpath("//button[@aria-label='Next page']")));
+    }
+
+    @Override
+    public void goToPreviousPage() {
+        ((JavascriptExecutor) getDriver())
+                .executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        click(elementToBeClickable(getDriver(), By.xpath("//button[@aria-label='Previous page']")));
+    }
+
+    @Override
     public WebElement getNewBusinessTermButton() {
         return elementToBeClickable(getDriver(), NEW_BUSINESS_TERM_BUTTON_LOCATOR);
     }
@@ -52,6 +182,22 @@ public class ViewEditBusinessTermPageImpl extends BasePageImpl implements ViewEd
     }
 
     @Override
+    public WebElement getUploadBusinessTermsButton() {
+        return elementToBeClickable(getDriver(), UPLOAD_BUSINESS_TERMS_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public void hitUploadBusinessTermsButton() {
+        click(getUploadBusinessTermsButton());
+        invisibilityOfLoadingContainerElement(getDriver());
+    }
+
+    @Override
+    public WebElement getDiscardButton() {
+        return elementToBeClickable(getDriver(), DISCARD_BUTTON_LOCATOR);
+    }
+
+    @Override
     public CreateBusinessTermPage openCreateBusinessTermPage() {
         click(getNewBusinessTermButton());
         CreateBusinessTermPage createBusinessTermPage = new CreateBusinessTermPageImpl(this);
@@ -60,30 +206,30 @@ public class ViewEditBusinessTermPageImpl extends BasePageImpl implements ViewEd
     }
 
     @Override
-    public EditBusinessTermPage openEditBusinessTermPageByTerm(String businessTermName) throws NoSuchElementException{
-        setName(contextCategoryName);
+    public EditBusinessTermPage openEditBusinessTermPageByTerm(String businessTermName) throws NoSuchElementException {
+        setTerm(businessTermName);
         hitSearchButton();
 
         return retry(() -> {
             WebElement td;
             try {
                 WebElement tr = getTableRecordAtIndex(1);
-                td = getColumnByName(tr, "name");
+                td = getColumnByName(tr, "Term");
             } catch (TimeoutException e) {
-                throw new NoSuchElementException("Cannot locate a context category using " + contextCategoryName, e);
+                throw new NoSuchElementException("Cannot locate a business term using " + businessTermName, e);
             }
-            if (!contextCategoryName.equals(getText(td.findElement(By.cssSelector("a > span"))))) {
-                throw new NoSuchElementException("Cannot locate a context category using " + contextCategoryName);
+            if (!businessTermName.equals(getText(td.findElement(By.cssSelector("a > span"))))) {
+                throw new NoSuchElementException("Cannot locate a business term using " + businessTermName);
             }
             WebElement tdName = td.findElement(By.tagName("a"));
             click(tdName);
 
-            ContextCategoryObject contextCategory =
-                    getAPIFactory().getContextCategoryAPI().getContextCategoryByName(contextCategoryName);
-            EditContextCategoryPage editContextCategoryPage =
-                    new EditContextCategoryPageImpl(this, contextCategory);
-            assert editContextCategoryPage.isOpened();
-            return editContextCategoryPage;
+            BusinessTermObject businessTerm =
+                    getAPIFactory().getBusinessTermAPI().getBusinessTermByName(businessTermName);
+            EditBusinessTermPage editBusinessTermPage =
+                    new EditBusinessTermPageImpl(this, businessTerm);
+            assert editBusinessTermPage.isOpened();
+            return editBusinessTermPage;
         });
     }
 
