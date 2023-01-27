0e13c0fc027815e96e5ac1efcc12dc4c49d1db86
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-01-27 16:48:16 -0500

    update AssignedBusinessTermObject

--- score-e2e/src/main/java/org/oagi/score/e2e/obj/AssignedBusinessTermObject.java
+++ score-e2e/src/main/java/org/oagi/score/e2e/obj/AssignedBusinessTermObject.java
@@ -2,7 +2,9 @@ package org.oagi.score.e2e.obj;
 
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Data
 public class AssignedBusinessTermObject {
 
@@ -15,7 +17,27 @@ public class AssignedBusinessTermObject {
     private String businessTermId;
     private String businessTerm;
     private String externalReferenceUri;
-    private Date lastUpdateTimestamp;
+
+    private LocalDateTime creationTimestamp;
+    private LocalDateTime lastUpdateTimestamp;
     private String owner;
     private String lastUpdateUser;
+
+    public static AssignedBusinessTermObject createRandomAssignedBusinessTerm(AppUserObject creator, String bieId,String bieType, String den, String businessTermId, String businessTerm, String externalReferenceUri, String typeCode, Boolean isPrimary) {
+        AssignedBusinessTermObject assignedBusinessTerm = new AssignedBusinessTermObject();
+        assignedBusinessTerm.setAssignedBizTermId(UUID.randomUUID().toString().replaceAll("-", ""));
+        assignedBusinessTerm.setBieId(bieId);
+        assignedBusinessTerm.setBieType(bieType);
+        assignedBusinessTerm.setDen(den);
+        assignedBusinessTerm.setBusinessTermId(businessTermId);
+        assignedBusinessTerm.setBusinessTerm(businessTerm);
+        assignedBusinessTerm.setExternalReferenceUri(externalReferenceUri);
+        assignedBusinessTerm.setIsPrimary(isPrimary);
+        assignedBusinessTerm.setTypeCode(typeCode);
+        assignedBusinessTerm.setOwner(creator.getAppUserId().toString());
+        assignedBusinessTerm.setLastUpdateUser(creator.getAppUserId().toString());
+        assignedBusinessTerm.setCreationTimestamp(LocalDateTime.now());
+        assignedBusinessTerm.setLastUpdateTimestamp(LocalDateTime.now());
+        return assignedBusinessTerm;
+    }
 }
