36d23dc994acb46b56e847fa373683fb861426d2
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-02-06 13:37:13 -0500

    Revert "Revert "Issue #1386 - Core Component Management for developer""
    This reverts commit 7a25ea9f8aa47206b571bc5b64f53f3c07453a46.

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_6_EndUserAccessRightScoreCoreFunctions/TC_6_2_EndUserAuthorizedManagementBIE.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_6_EndUserAccessRightScoreCoreFunctions/TC_6_2_EndUserAuthorizedManagementBIE.java
@@ -2073,7 +2073,6 @@ public class TC_6_2_EndUserAuthorizedManagementBIE extends BaseTest {
             ACCObject acc = coreComponentAPI.createRandomACCSemanticGroupType(endUserForCC, release, namespace, "Published");
             asccp = coreComponentAPI.createRandomASCCP(acc, endUserForCC, namespace, "Published");
             context = getAPIFactory().getBusinessContextAPI().createRandomBusinessContext(usera);
-            ;
         }
 
         HomePage homePage = loginPage().signIn(usera.getLoginId(), usera.getPassword());
@@ -2111,7 +2110,6 @@ public class TC_6_2_EndUserAuthorizedManagementBIE extends BaseTest {
             asccp = coreComponentAPI.createRandomASCCP(acc, endUserForCC, namespace, "Published");
 
             context = getAPIFactory().getBusinessContextAPI().createRandomBusinessContext(usera);
-            ;
         }
 
         HomePage homePage = loginPage().signIn(usera.getLoginId(), usera.getPassword());
