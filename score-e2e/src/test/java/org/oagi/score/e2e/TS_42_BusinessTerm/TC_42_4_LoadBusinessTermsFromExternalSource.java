03b3f03589bdf585663a2f5c376a2044776948e2
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-02-09 14:00:33 -0500

    update TC_42_4_4 & TC42_4_5

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_42_BusinessTerm/TC_42_4_LoadBusinessTermsFromExternalSource.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_42_BusinessTerm/TC_42_4_LoadBusinessTermsFromExternalSource.java
@@ -16,6 +16,7 @@ import org.oagi.score.e2e.obj.AppUserObject;
 import org.oagi.score.e2e.page.HomePage;
 import org.oagi.score.e2e.page.business_term.UploadBusinssTermsPage;
 import org.oagi.score.e2e.page.business_term.ViewEditBusinessTermPage;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 import java.io.BufferedWriter;
@@ -26,6 +27,8 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.oagi.score.e2e.impl.PageHelper.click;
 
 @Execution(ExecutionMode.CONCURRENT)
@@ -106,11 +109,15 @@ public class TC_42_4_LoadBusinessTermsFromExternalSource extends BaseTest {
         chooseFile.sendKeys(csvFileForUpload.getAbsolutePath());
         //Verify that all test business terms have been saved through bulk upload
         ViewEditBusinessTermPage viewEditBusinessTermPageForCheck = homePage.getBIEMenu().openViewEditBusinessTermSubMenu();
-
-
-
-
-
+        viewEditBusinessTermPageForCheck.setTerm("bt_bulk_upload1");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        viewEditBusinessTermPageForCheck.setExternalReferenceURI("http://btupload2.com");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        viewEditBusinessTermPageForCheck.setTerm("bt_bulk_upload3");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
     }
 
     @Test
@@ -121,11 +128,94 @@ public class TC_42_4_LoadBusinessTermsFromExternalSource extends BaseTest {
     @Test
     @DisplayName("TC_42_4_4")
     public void new_business_term_will_be_created_if_the_business_term_is_uploaded_with_new_external_reference_uri() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        BIEMenu bieMenu = homePage.getBIEMenu();
+        ViewEditBusinessTermPage viewEditBusinessTermPage = bieMenu.openViewEditBusinessTermSubMenu();
+        UploadBusinssTermsPage uploadBusinssTermsPage = viewEditBusinessTermPage.hitUploadBusinessTermsButton();
+
+        File targetFolder = new File(System.getProperty("user.home"), "Downloads");
+
+        //write test business terms into csv file and save into a different name for upload
+        File csvFileForUpload = new File(targetFolder, "businessTermTemplateWithExampleForUpload.csv");
+        try(
+                BufferedWriter writer = Files.newBufferedWriter(csvFileForUpload.toPath());
+
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                        .withHeader("businessTerm", "externalReferenceUri", "externalReferenceId", "definition", "comment"));
+        ){
+            csvPrinter.printRecord("bt_bulk_upload1", "http://btupload1.com", "1", "business term 1 through bulk upload","business term 1 through bulk upload" );
+            csvPrinter.printRecord("bt_bulk_upload2", "http://btupload2.com", "2", "business term 2 through bulk upload","business term 2 through bulk upload" );
+            csvPrinter.printRecord("bt_bulk_upload3", "http://btupload3.com", "3", "business term 3 through bulk upload","business term 3 through bulk upload");
+
+            csvPrinter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //upload the modified csv file
+        WebElement chooseFile = click(uploadBusinssTermsPage.getAttachButton());
+        chooseFile.sendKeys(csvFileForUpload.getAbsolutePath());
+        //Verify that all test business terms have been saved through bulk upload
+        ViewEditBusinessTermPage viewEditBusinessTermPageForCheck = homePage.getBIEMenu().openViewEditBusinessTermSubMenu();
+        viewEditBusinessTermPageForCheck.setTerm("bt_bulk_upload1");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        viewEditBusinessTermPageForCheck.setExternalReferenceURI("http://btupload2.com");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        viewEditBusinessTermPageForCheck.setTerm("bt_bulk_upload3");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
     }
 
     @Test
     @DisplayName("TC_42_4_5")
     public void previous_business_term_will_be_updated_with_new_information_if_the_business_term_is_uploaded_with_an_exitent_external_reference_uri() {
+
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        BIEMenu bieMenu = homePage.getBIEMenu();
+        ViewEditBusinessTermPage viewEditBusinessTermPage = bieMenu.openViewEditBusinessTermSubMenu();
+        UploadBusinssTermsPage uploadBusinssTermsPage = viewEditBusinessTermPage.hitUploadBusinessTermsButton();
+
+        File targetFolder = new File(System.getProperty("user.home"), "Downloads");
+
+        //write test business terms into csv file and save into a different name for upload
+        File csvFileForUpload = new File(targetFolder, "businessTermTemplateWithExampleForUpload.csv");
+        try(
+                BufferedWriter writer = Files.newBufferedWriter(csvFileForUpload.toPath());
+
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                        .withHeader("businessTerm", "externalReferenceUri", "externalReferenceId", "definition", "comment"));
+        ){
+            csvPrinter.printRecord("bt_bulk_upload1", "http://btupload1.com", "1", "business term 1 through bulk upload","business term 1 through bulk upload" );
+            csvPrinter.printRecord("bt_bulk_upload2", "http://btupload2.com", "2", "business term 2 through bulk upload","business term 2 through bulk upload" );
+            csvPrinter.printRecord("bt_bulk_upload3", "http://btupload1.com", "3", "business term 3 through bulk upload","business term 3 through bulk upload");
+
+            csvPrinter.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //upload the modified csv file
+        WebElement chooseFile = click(uploadBusinssTermsPage.getAttachButton());
+        chooseFile.sendKeys(csvFileForUpload.getAbsolutePath());
+        //Verify that all test business terms have been saved through bulk upload
+        ViewEditBusinessTermPage viewEditBusinessTermPageForCheck = homePage.getBIEMenu().openViewEditBusinessTermSubMenu();
+        viewEditBusinessTermPageForCheck.setTerm("bt_bulk_upload3");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        viewEditBusinessTermPageForCheck.setExternalReferenceURI("http://btupload2.com");
+        viewEditBusinessTermPageForCheck.hitSearchButton();
+        assertTrue(viewEditBusinessTermPageForCheck.getSelectCheckboxAtIndex(1).isDisplayed());
+        assertThrows(NoSuchElementException.class, () -> {
+            viewEditBusinessTermPageForCheck.openEditBusinessTermPageByTerm("bt_bulk_upload1");
+        });
     }
 
     @AfterEach
