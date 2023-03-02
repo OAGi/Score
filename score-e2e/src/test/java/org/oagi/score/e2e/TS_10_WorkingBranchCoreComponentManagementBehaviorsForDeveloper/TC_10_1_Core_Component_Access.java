97dc07a995eb97567764968d4d960811fbc69224
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-02 14:31:59 -0500

    add TA10_1_16

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/TC_10_1_Core_Component_Access.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/TC_10_1_Core_Component_Access.java
@@ -1141,6 +1141,25 @@ public class TC_10_1_Core_Component_Access extends BaseTest {
     @DisplayName("TC_10_1_TA_16")
     public void test_TA_16(){
 
+        AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(true);
+        thisAccountWillBeDeletedAfterTests(developer);
+
+        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        CoreComponentMenu coreComponentMenu = homePage.getCoreComponentMenu();
+        ViewEditCoreComponentPage viewEditCoreComponentPage = coreComponentMenu.openViewEditCoreComponentSubMenu();
+        viewEditCoreComponentPage.setModule("Model\\Platform\\2_6\\Common\\Components\\Components");
+        click(viewEditCoreComponentPage.getSearchButton());
+        assertTrue(viewEditCoreComponentPage.getTableRecordByCCNameAndOwner("ASN Reference. Document Reference", "oagis").isDisplayed());
+        assertTrue(viewEditCoreComponentPage.getTableRecordByCCNameAndOwner("Show Receive Delivery. Show Receive Delivery", "oagis").isDisplayed());
+
+        viewEditCoreComponentPage.openPage();
+        viewEditCoreComponentPage.setDefinition("\"Notice Document\"");
+        click(viewEditCoreComponentPage.getSearchButton());
+        assertTrue(viewEditCoreComponentPage.getTableRecordByCCNameAndOwner("ASN Reference. Document Reference", "oagis").isDisplayed());
+        assertFalse(viewEditCoreComponentPage.getTableRecordByCCNameAndOwner("Show Receive Delivery. Show Receive Delivery", "oagis").isDisplayed());
+
+
+
 
 
 
