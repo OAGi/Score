9884a2ba66f43ef273c3292c5a4785a25339edf0
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-03 15:55:52 -0500

    update TA10_1_1

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/TC_10_1_Core_Component_Access.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/TC_10_1_Core_Component_Access.java
@@ -42,128 +42,6 @@ public class TC_10_1_Core_Component_Access extends BaseTest {
     private void thisAccountWillBeDeletedAfterTests(AppUserObject appUser) {
         this.randomAccounts.add(appUser);
     }
-
-    private class RandomCoreComponentContainer {
-        private AppUserObject appUser;
-        private int yieldPointer = 0;
-        int numberOfWIPCCs;
-        int numberOfDraftCCs;
-        int numberOfCandidateCCs;
-
-        private List<ACCObject> arrayWIPACCs = new ArrayList<>();
-        private List<ASCCPObject> arrayWIPASCCPs = new ArrayList<>();
-        private List<BCCPObject> arrayWIPBCCPs = new ArrayList<>();
-        private List<ACCObject> arrayDraftACCs = new ArrayList<>();
-        private List<ASCCPObject> arrayDraftASCCPs = new ArrayList<>();
-        private List<BCCPObject> arrayDraftBCCPs = new ArrayList<>();
-
-        private List<ACCObject> arrayCandidateACCs = new ArrayList<>();
-        private List<ASCCPObject> arrayCandidateASCCPs = new ArrayList<>();
-        private List<BCCPObject> arrayCandidateBCCPs = new ArrayList<>();
-        public RandomCoreComponentContainer(AppUserObject appUser, ReleaseObject release,NamespaceObject namespace) {
-            this(appUser, release, namespace, nextInt(1, 3), nextInt(1, 3), nextInt(1, 3));
-        }
-
-        public RandomCoreComponentContainer(AppUserObject appUser, ReleaseObject release, NamespaceObject namespace,
-                                           int numberOfWIPCCs, int numberOfDraftCCs, int numberOfCandidateCCs) {
-            this.appUser = appUser;
-            this.numberOfWIPCCs = numberOfWIPCCs;
-            this.numberOfDraftCCs = numberOfDraftCCs;
-            this.numberOfCandidateCCs = numberOfCandidateCCs;
-
-            AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(true);
-            thisAccountWillBeDeletedAfterTests(developer);
-
-            for (int i = 0; i < numberOfWIPCCs; ++i) {
-                ASCCPObject asccp;
-                BCCPObject bccp;
-                ACCObject acc;
-
-                {
-                    CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
-
-                    acc = coreComponentAPI.createRandomACC(developer, release, namespace, "WIP");
-                    DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
-                    bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "WIP");
-                    BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "WIP");
-                    bcc.setCardinalityMax(1);
-                    coreComponentAPI.updateBCC(bcc);
-
-                    ACCObject acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "WIP");
-                    BCCPObject bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "WIP");
-                    coreComponentAPI.appendBCC(acc_association, bccp_to_append, "WIP");
-
-                    asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "WIP");
-                    ASCCObject ascc = coreComponentAPI.appendASCC(acc, asccp, "WIP");
-                    ascc.setCardinalityMax(1);
-                    coreComponentAPI.updateASCC(ascc);
-                    arrayWIPACCs.add(acc);
-                    arrayWIPASCCPs.add(asccp);
-                    arrayWIPBCCPs.add(bccp);
-                }
-            }
-            for (int i = 0; i < numberOfDraftCCs; ++i) {
-                ASCCPObject asccp;
-                BCCPObject bccp;
-                ACCObject acc;
-
-                {
-                    CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
-
-                    acc = coreComponentAPI.createRandomACC(developer, release, namespace, "Draft");
-                    DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
-                    bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Draft");
-                    BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "Draft");
-                    bcc.setCardinalityMax(1);
-                    coreComponentAPI.updateBCC(bcc);
-
-                    ACCObject acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "Draft");
-                    BCCPObject bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Draft");
-                    coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Draft");
-
-                    asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "Draft");
-                    ASCCObject ascc = coreComponentAPI.appendASCC(acc, asccp, "Draft");
-                    ascc.setCardinalityMax(1);
-                    coreComponentAPI.updateASCC(ascc);
-                    arrayDraftACCs.add(acc);
-                    arrayDraftASCCPs.add(asccp);
-                    arrayDraftBCCPs.add(bccp);
-                }
-
-            }
-            for (int i = 0; i < numberOfCandidateCCs; ++i) {
-                ASCCPObject asccp;
-                BCCPObject bccp;
-                ACCObject acc;
-
-                {
-                    CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
-
-                    acc = coreComponentAPI.createRandomACC(developer, release, namespace, "Candidate");
-                    DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
-                    bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Candidate");
-                    BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "Candidate");
-                    bcc.setCardinalityMax(1);
-                    coreComponentAPI.updateBCC(bcc);
-
-                    ACCObject acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "Candidate");
-                    BCCPObject bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Candidate");
-                    coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Candidate");
-
-                    asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "Candidate");
-                    ASCCObject ascc = coreComponentAPI.appendASCC(acc, asccp, "Candidate");
-                    ascc.setCardinalityMax(1);
-                    coreComponentAPI.updateASCC(ascc);
-                    arrayCandidateACCs.add(acc);
-                    arrayCandidateASCCPs.add(asccp);
-                    arrayCandidateBCCPs.add(bccp);
-                }
-
-            }
-        }
-
-    }
-
     private class RandomCoreComponentWithStateContainer {
         private AppUserObject appUser;
 
@@ -217,8 +95,11 @@ public class TC_10_1_Core_Component_Access extends BaseTest {
 
         ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
         NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
-
-        RandomCoreComponentContainer randomCoreComponentContainer = new RandomCoreComponentContainer(developer, release, namespace);
+        List<String> ccStates = new ArrayList<>();
+        ccStates.add("WIP");
+        ccStates.add("Draft");
+        ccStates.add("Candidate");
+        RandomCoreComponentWithStateContainer randomCoreComponentWithStateContainer = new RandomCoreComponentWithStateContainer(developer, release, namespace, ccStates);
 
         AppUserObject developer2 = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(true);
         thisAccountWillBeDeletedAfterTests(developer2);
@@ -226,63 +107,25 @@ public class TC_10_1_Core_Component_Access extends BaseTest {
         HomePage homePage = loginPage().signIn(developer2.getLoginId(), developer2.getPassword());
         CoreComponentMenu coreComponentMenu = homePage.getCoreComponentMenu();
         ViewEditCoreComponentPage viewEditCoreComponentPage = coreComponentMenu.openViewEditCoreComponentSubMenu();
-        int numOfWIPACCs = randomCoreComponentContainer.arrayWIPACCs.size();
-        for(int i=0; i < numOfWIPACCs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayWIPACCs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
-        int numOfWIPASCCPs = randomCoreComponentContainer.arrayWIPASCCPs.size();
-        for(int i=0; i < numOfWIPASCCPs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayWIPASCCPs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
 
-        int numOfWIPBCCPs = randomCoreComponentContainer.arrayWIPBCCPs.size();
-        for(int i=0; i < numOfWIPBCCPs; i++){
+        for (Map.Entry<String, ACCObject> entry: randomCoreComponentWithStateContainer.stateACCs.entrySet()) {
+            ACCObject acc;
+            ASCCPObject asccp;
+            BCCPObject bccp;
+            String state = entry.getKey();
+            acc = entry.getValue();
+            asccp = randomCoreComponentWithStateContainer.stateASCCPs.get(state);
+            bccp = randomCoreComponentWithStateContainer.stateBCCPs.get(state);
             viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayWIPBCCPs.get(i).getDen());
+            viewEditCoreComponentPage.setDEN(acc.getDen());
             assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
 
-        int numOfDraftACCs = randomCoreComponentContainer.arrayDraftACCs.size();
-        for(int i=0; i < numOfDraftACCs; i++){
             viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayDraftACCs.get(i).getDen());
+            viewEditCoreComponentPage.setDEN(asccp.getDen());
             assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
 
-        int numOfDraftASCCPs = randomCoreComponentContainer.arrayDraftASCCPs.size();
-        for(int i=0; i < numOfDraftASCCPs; i++){
             viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayDraftASCCPs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
-
-        int numOfDraftBCCPs = randomCoreComponentContainer.arrayDraftBCCPs.size();
-        for(int i=0; i < numOfDraftBCCPs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayDraftBCCPs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
-
-        int numOfCandidateACCs = randomCoreComponentContainer.arrayCandidateACCs.size();
-        for(int i=0; i < numOfCandidateACCs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayCandidateACCs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
-        int numOfCandidateASCCPs = randomCoreComponentContainer.arrayCandidateASCCPs.size();
-        for(int i=0; i < numOfCandidateASCCPs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayCandidateASCCPs.get(i).getDen());
-            assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
-        }
-        int numOfCandidateBCCPs = randomCoreComponentContainer.arrayCandidateBCCPs.size();
-        for(int i=0; i < numOfCandidateBCCPs; i++){
-            viewEditCoreComponentPage.openPage();
-            viewEditCoreComponentPage.setDEN(randomCoreComponentContainer.arrayCandidateBCCPs.get(i).getDen());
+            viewEditCoreComponentPage.setDEN(bccp.getDen());
             assertTrue(viewEditCoreComponentPage.getTableRecordAtIndex(1).isDisplayed());
         }
     }
