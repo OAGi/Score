fedab650c855ad3267ee42eec9cdfc02641e1a0a
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-03-10 11:04:02 -0500

    Implementation of the TC_6_3_TA_38 assertion

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_6_EndUserAccessRightScoreCoreFunctions/TC_6_3_EndUserAuthorizedAccessToBIEExpressionGeneration.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_6_EndUserAccessRightScoreCoreFunctions/TC_6_3_EndUserAuthorizedAccessToBIEExpressionGeneration.java
@@ -2457,6 +2457,59 @@ public class TC_6_3_EndUserAuthorizedAccessToBIEExpressionGeneration extends Bas
 
         }
     }
+    @Test
+    @DisplayName("TC_6_3_TA_38")
+    public void test_TA_38() {
+        AppUserObject usera;
+        AppUserObject userb;
+        ArrayList<TopLevelASBIEPObject> biesForTesting = new ArrayList<>();
+        ASCCPObject asccp;
+        BusinessContextObject context;
+        {
+            ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber(this.release);
+            usera = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+            userb = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+
+            thisAccountWillBeDeletedAfterTests(usera);
+            thisAccountWillBeDeletedAfterTests(userb);
+
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+            NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+
+            ACCObject acc = coreComponentAPI.createRandomACC(userb, release, namespace, "Published");
+            asccp = coreComponentAPI.createRandomASCCP(acc, userb, namespace, "Published");
+
+            context = getAPIFactory().getBusinessContextAPI().createRandomBusinessContext(userb);
+            TopLevelASBIEPObject useraBIEProduction = getAPIFactory().getBusinessInformationEntityAPI().
+                    generateRandomTopLevelASBIEP(Arrays.asList(context), asccp, userb, "Production");
+            biesForTesting.add(useraBIEProduction);
+        }
+
+        HomePage homePage = loginPage().signIn(usera.getLoginId(), usera.getPassword());
+        BIEMenu bieMenu = homePage.getBIEMenu();
+        getDriver().manage().window().maximize();
+
+        ExpressBIEPage expressBIEPage = bieMenu.openExpressBIESubMenu();
+        for (TopLevelASBIEPObject bie : biesForTesting) {
+            assertDoesNotThrow(() -> {
+                expressBIEPage.selectBIEForExpression(bie);
+            });
+            ExpressBIEPage.OpenAPIExpressionOptions openAPIExpressionOptions = expressBIEPage.selectOpenAPIExpression();
+            openAPIExpressionOptions.selectYAMLOpenAPIFormat();
+            ExpressBIEPage.OpenAPIExpressionPOSTOperationOptions postOperationOptions = openAPIExpressionOptions.togglePOSTOperationTemplate();
+            postOperationOptions.toggleMakeAsAnArray();
+            expressBIEPage.selectPutAllSchemasInTheSameFile();
+
+            File file = null;
+            try {
+                file = expressBIEPage.hitGenerateButton(ExpressBIEPage.ExpressionFormat.YML);
+            } finally {
+                if (file != null) {
+                    file.delete();
+                }
+            }
+        }
+    }
 
     @AfterEach
     public void tearDown() {
