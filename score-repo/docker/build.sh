bd05450c97c57fb885f5c14c4a9e1df66068106a
Hakju Oh <hakju.oh@nist.gov>
2023-03-13 15:43:04 -0400

    - Update Dockerfile - Add scripts to build docker images - Add the DB schema migration script

---
+++ score-repo/docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if ! [[ -x "$(command -v docker)" ]]; then
+  echo "Error: docker is not installed. See https://docs.docker.com/install/ for details.'" >&2
+  exit 1
+fi
+
+echo "Building docker image..."
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-repo:3.0.0-beta .
+
+echo "Scanning vulnerabilities..."
+docker scan -f Dockerfile oagi1docker/srt-repo:3.0.0-beta
+
+echo "Done."
