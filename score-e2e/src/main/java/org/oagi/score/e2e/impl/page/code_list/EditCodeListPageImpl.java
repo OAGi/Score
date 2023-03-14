a8c7bc68788e1c8b679884f597092e8e5b82dbda
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-03-14 12:21:45 -0400

    Implementation of the TC_11_1_TA_2 assertion

--- score-e2e/src/main/java/org/oagi/score/e2e/impl/page/code_list/EditCodeListPageImpl.java
+++ score-e2e/src/main/java/org/oagi/score/e2e/impl/page/code_list/EditCodeListPageImpl.java
@@ -2,10 +2,10 @@ package org.oagi.score.e2e.impl.page.code_list;
 
 import org.oagi.score.e2e.impl.PageHelper;
 import org.oagi.score.e2e.impl.page.BasePageImpl;
-import org.oagi.score.e2e.obj.ACCObject;
 import org.oagi.score.e2e.obj.CodeListObject;
 import org.oagi.score.e2e.page.BasePage;
 import org.oagi.score.e2e.page.code_list.EditCodeListPage;
+import org.oagi.score.e2e.page.code_list.EditCodeListValueDialog;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -15,6 +15,15 @@ import static java.time.Duration.ofMillis;
 import static org.oagi.score.e2e.impl.PageHelper.*;
 
 public class EditCodeListPageImpl extends BasePageImpl implements EditCodeListPage {
+
+    private static final By DEFINITION_SOURCE_FIELD_LOCATOR =
+            By.xpath("//mat-label[contains(text(), \"Definition Source\")]//ancestor::mat-form-field//input");
+    private static final By DEFINITION_FIELD_LOCATOR =
+            By.xpath("//mat-label[contains(text(), \"Definition\")]//ancestor::mat-form-field//textarea");
+    private static final By UPDATE_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Update\")]//ancestor::button[1]");
+    private static final By ADD_CODE_LIST_VALUE_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Add\")]//ancestor::button[1]");
     private final CodeListObject codeList;
 
     public EditCodeListPageImpl(BasePage parent, CodeListObject codeList) {
@@ -40,5 +49,49 @@ public class EditCodeListPageImpl extends BasePageImpl implements EditCodeListPa
         invisibilityOfLoadingContainerElement(getDriver());
         return visibilityOfElementLocated(PageHelper.wait(getDriver(), Duration.ofSeconds(10L), ofMillis(100L)),
                 By.xpath("//mat-card-title/span[1]"));
+    }
+
+    @Override
+    public void setDefinition(String definition) {
+        sendKeys(getDefinitionField(), definition);
+    }
+
+    @Override
+    public void setDefinitionSource(String definitionSource) {
+        sendKeys(getDefinitionSourceField(), definitionSource);
+    }
+
+    @Override
+    public WebElement getDefinitionField() {
+        return visibilityOfElementLocated(getDriver(), DEFINITION_FIELD_LOCATOR);
+    }
+    @Override
+    public WebElement getDefinitionSourceField() {
+        return visibilityOfElementLocated(getDriver(), DEFINITION_SOURCE_FIELD_LOCATOR);
+    }
+    @Override
+    public void hitUpdateButton() {
+        retry(() -> {
+            click(getUpdateButton());
+            waitFor(ofMillis(1000L));
+        });
+        invisibilityOfLoadingContainerElement(getDriver());
+    }
+    @Override
+    public WebElement getUpdateButton() {
+        return elementToBeClickable(getDriver(), UPDATE_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public EditCodeListValueDialog addCodeListValue() {
+        click(getAddCodeListValueButton());
+        EditCodeListValueDialog editCodeListValueDialog = new EditCodeListValueDialogImpl(this);
+        assert editCodeListValueDialog.isOpened();
+        return editCodeListValueDialog;
+    }
+
+    @Override
+    public WebElement getAddCodeListValueButton() {
+        return elementToBeClickable(getDriver(), ADD_CODE_LIST_VALUE_BUTTON_LOCATOR);
     }
 }
--- score-e2e/src/test/java/org/oagi/score/e2e/TS_11_WorkingBranchCodeListManagementForDeveloper/TC_11_1_CodeListAccess.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_11_WorkingBranchCodeListManagementForDeveloper/TC_11_1_CodeListAccess.java
@@ -1,20 +1,21 @@
 package org.oagi.score.e2e.TS_11_WorkingBranchCodeListManagementForDeveloper;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.oagi.score.e2e.BaseTest;
 import org.oagi.score.e2e.obj.*;
 import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.code_list.EditCodeListPage;
+import org.oagi.score.e2e.page.code_list.EditCodeListValueDialog;
 import org.oagi.score.e2e.page.code_list.ViewEditCodeListPage;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.oagi.score.e2e.AssertionHelper.assertDisabled;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_11_1_CodeListAccess extends BaseTest {
@@ -65,6 +66,58 @@ public class TC_11_1_CodeListAccess extends BaseTest {
         for (CodeListObject cl : codeListForTesting){
             assertNotEquals(developerB.getAppUserId(), cl.getOwnerUserId());
             viewEditCodeListPage.searchCodeListByNameAndBranch(cl.getName(), workingBranch.getReleaseNumber());
+        }
+
+    }
+
+    @Test
+    @DisplayName("TC_11_1_TA_2")
+    public void test_TA_2(){
+        ArrayList<CodeListObject> codeListForTesting = new ArrayList<>();
+        AppUserObject developerA;
+        ReleaseObject workingBranch;
+        {
+            developerA = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+            thisAccountWillBeDeletedAfterTests(developerA);
+            NamespaceObject namespace = getAPIFactory().getNamespaceAPI().createRandomDeveloperNamespace(developerA);
+            /**
+             * Create Code List for Working branch. States - WIP, Draft and Candidate
+             */
+            workingBranch = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+            CodeListObject codeListWIP = getAPIFactory().getCodeListAPI().createRandomCodeList(developerA, namespace, workingBranch, "WIP");
+            getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeListWIP, developerA);
+            codeListForTesting.add(codeListWIP);
+
+            CodeListObject codeListDraft = getAPIFactory().getCodeListAPI().createRandomCodeList(developerA, namespace, workingBranch, "Draft");
+            getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeListDraft, developerA);
+            codeListForTesting.add(codeListDraft);
+
+            CodeListObject codeListCandidate = getAPIFactory().getCodeListAPI().createRandomCodeList(developerA, namespace, workingBranch, "Candidate");
+            getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeListCandidate, developerA);
+            codeListForTesting.add(codeListCandidate);
+        }
+        HomePage homePage = loginPage().signIn(developerA.getLoginId(), developerA.getPassword());
+        getDriver().manage().window().maximize();
+        for (CodeListObject cl : codeListForTesting){
+            assertEquals(developerA.getAppUserId(), cl.getOwnerUserId());
+            assertEquals(Boolean.valueOf("true"), developerA.isDeveloper());
+            ViewEditCodeListPage viewEditCodeListPage = homePage.getCoreComponentMenu().openViewEditCodeListSubMenu();
+            EditCodeListPage editCodeListPage = viewEditCodeListPage.openCodeListViewEditPageByNameAndBranch(cl.getName(), workingBranch.getReleaseNumber());
+            if (cl.getState().equals("WIP")){
+                /**
+                 * The developer can view and edit the details (including code values) of a CL that is in the WIP state and owned by him.
+                 */
+                editCodeListPage.setDefinition("test definition");
+                editCodeListPage.setDefinitionSource("test definition source");
+                EditCodeListValueDialog editCodeListValueDialog = editCodeListPage.addCodeListValue();
+                editCodeListValueDialog.setCode("code");
+                editCodeListValueDialog.setMeaning("meaning");
+                editCodeListValueDialog.hitAddButton();
+                editCodeListPage.hitUpdateButton();
+            }else{
+                assertDisabled(editCodeListPage.getDefinitionField());
+                assertDisabled(editCodeListPage.getDefinitionSourceField());
+            }
         }
 
     }
