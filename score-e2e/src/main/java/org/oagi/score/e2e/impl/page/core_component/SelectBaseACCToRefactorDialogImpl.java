7bcd90de3da2598daa1850f080ff168d05576b3a
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-20 13:33:07 -0400

    add TA_10_4_13

---
+++ score-e2e/src/main/java/org/oagi/score/e2e/impl/page/core_component/SelectBaseACCToRefactorDialogImpl.java
@@ -0,0 +1,98 @@
+package org.oagi.score.e2e.impl.page.core_component;
+
+import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
+import org.oagi.score.e2e.page.core_component.SelectBaseACCToRefactorDialog;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.oagi.score.e2e.impl.PageHelper.*;
+
+public class SelectBaseACCToRefactorDialogImpl implements SelectBaseACCToRefactorDialog {
+
+    private static final By ANALYZE_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Analyze\")]//ancestor::button[1]");
+
+    private static final By CANCEL_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Cancel\")]//ancestor::button[1]");
+
+    private static final By REFACTOR_BUTTON_LOCATOR =
+            By.xpath("//mat-dialog-container//button//*[contains(text(),\"Refactor\")]");
+
+    private ACCViewEditPageImpl parent;
+    private String associationToRefactor;
+
+    public SelectBaseACCToRefactorDialogImpl(ACCViewEditPageImpl parent, String associationPropertyTerm) {
+        this.parent = parent;
+        this.associationToRefactor = associationPropertyTerm;
+    }
+
+    private WebDriver getDriver() {
+        return this.parent.getDriver();
+    }
+    @Override
+    public boolean isOpened() {
+        WebElement title;
+        try {
+            title = getTitle();
+        } catch (TimeoutException e) {
+            return false;
+        }
+        assert ("Select a base ACC to move " + this.associationToRefactor).equals(getText(title));
+        return true;
+    }
+
+    @Override
+    public WebElement getTitle() {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//mat-dialog-container//span[contains(@class, \"title\")]"));
+    }
+
+    @Override
+    public WebElement getTableRecordAtIndex(int idx) {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//mat-dialog-container//tbody/tr[" + idx + "]"));
+    }
+
+    @Override
+    public WebElement getTableRecordByValue(String value) {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//mat-dialog-container//td//span[contains(text(), \"" + value + "\")]/ancestor::tr"));
+    }
+
+    @Override
+    public WebElement getColumnByName(WebElement tableRecord, String columnName) {
+        return tableRecord.findElement(By.className("mat-column-" + columnName));
+    }
+
+    @Override
+    public WebElement getCancelButton() {
+        return elementToBeClickable(getDriver(), CANCEL_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public ACCViewEditPage hitCancelButton() {
+        click(getCancelButton());
+        return parent;
+    }
+
+    @Override
+    public WebElement getAnalyzeButton() {
+        return elementToBeClickable(getDriver(), ANALYZE_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public SelectBaseACCToRefactorDialog hitAnalyzeButton() {
+        click(getAnalyzeButton());
+        return this;
+    }
+
+    @Override
+    public WebElement getRefactorButton() {
+        return elementToBeClickable(getDriver(), REFACTOR_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public ACCViewEditPage hitRefactorButton() {
+        click(getRefactorButton());
+        return parent;
+    }
+}
