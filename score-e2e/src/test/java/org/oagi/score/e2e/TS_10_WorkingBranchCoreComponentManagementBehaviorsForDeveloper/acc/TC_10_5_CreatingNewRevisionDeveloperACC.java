eb6fa8ee3cdbacb99675172c6206a88490d13dff
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-22 16:11:15 -0400

    update TA_10_5_1

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_5_CreatingNewRevisionDeveloperACC.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_5_CreatingNewRevisionDeveloperACC.java
@@ -6,10 +6,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.oagi.score.e2e.BaseTest;
+import org.oagi.score.e2e.obj.ACCObject;
 import org.oagi.score.e2e.obj.AppUserObject;
+import org.oagi.score.e2e.obj.NamespaceObject;
+import org.oagi.score.e2e.obj.ReleaseObject;
+import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
+import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.oagi.score.e2e.AssertionHelper.assertDisabled;
+import static org.oagi.score.e2e.AssertionHelper.assertNotChecked;
+import static org.oagi.score.e2e.impl.PageHelper.escape;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_10_5_CreatingNewRevisionDeveloperACC extends BaseTest {
@@ -38,9 +52,30 @@ public class TC_10_5_CreatingNewRevisionDeveloperACC extends BaseTest {
     @Test
     public void test_TA_10_5_1() {
 
+        AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+        thisAccountWillBeDeletedAfterTests(developer);
 
+        String branch = "Working";
+        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
 
-
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+        ACCObject acc = getAPIFactory().getCoreComponentAPI().createRandomACC(developer, release, namespace, "Published");
+        ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        accViewEditPage.hitReviseButton();
+        WebElement accNode = accViewEditPage.getNodeByPath("/" + acc.getDen());
+        ACCViewEditPage.ACCPanel accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals(acc.getObjectClassTerm(), getText(accPanel.getObjectClassTermField()));
+        assertEquals(acc.getDen(), getText(accPanel.getDENField()));
+        assertEquals(acc.getDefinition(), getText(accPanel.getDefinitionField()));
+        assertEquals(acc.getDefinitionSource(), getText(accPanel.getDefinitionSourceField()));
+        assertEquals(acc.getNamespaceId(), getText(accPanel.getNamespaceSelectField()));
+        assertNotChecked(accPanel.getAbstractCheckbox());
+        assertNotChecked(accPanel.getDeprecatedCheckbox());
+        assertDisabled(accPanel.getComponentTypeSelectField());
+        assertEquals("2", getText(accPanel.getRevisionField()));
     }
 
 
