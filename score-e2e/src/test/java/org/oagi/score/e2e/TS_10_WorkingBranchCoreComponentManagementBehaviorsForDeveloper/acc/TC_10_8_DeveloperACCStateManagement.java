d9d1beb6a8864a73964b478d53db029264a9d75f
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-28 10:34:51 -0400

    add TA_10_8_3 & 4

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_8_DeveloperACCStateManagement.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_8_DeveloperACCStateManagement.java
@@ -164,14 +164,118 @@ public class TC_10_8_DeveloperACCStateManagement extends BaseTest {
 
     @Test
     public void test_TA_10_8_3() {
+        AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+        thisAccountWillBeDeletedAfterTests(developer);
 
+        String branch = "Working";
+        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
 
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+        ACCObject acc, acc_association, accForBase;
+        ASCCObject ascc;
+        ASCCPObject asccp;
+        BCCPObject bccp, bccp_to_append;
+
+        {
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            acc = coreComponentAPI.createRandomACC(developer, release, namespace, "Candidate");
+            coreComponentAPI.updateACC(acc);
+
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Candidate");
+            BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "Candidate");
+            bcc.setCardinalityMax(1);
+            coreComponentAPI.updateBCC(bcc);
+
+            acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "Candidate");
+            bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Candidate");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Candidate");
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "Candidate");
+            ascc = coreComponentAPI.appendASCC(acc, asccp, "Candidate");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+        }
+        ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        WebElement accNode = accViewEditPage.getNodeByPath("/" + acc.getDen());
+        ACCViewEditPage.ACCPanel accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals("1", getText(accPanel.getRevisionField()));
+        assertEquals("Candidate", getText(accPanel.getStateField()));
+        accViewEditPage.backToWIP();
+        accViewEditPage.hitUpdateButton();
+
+        viewEditCoreComponentPage.openPage();
+        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals("WIP", getText(accPanel.getStateField()));
+        WebElement asccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + asccp.getPropertyTerm());
+        ACCViewEditPage.ASCCPPanel asccpPanel = accViewEditPage.getASCCPanelContainer(asccNode).getASCCPPanel();
+        assertEquals("WIP", getText(asccpPanel.getStateField()));
+        WebElement bccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + bccp_to_append.getPropertyTerm());
+        ACCViewEditPage.BCCPPanel bccpPanel = accViewEditPage.getBCCPanelContainer(bccNode).getBCCPPanel();
+        assertEquals("WIP", getText(bccpPanel.getStateField()));
     }
 
     @Test
     public void test_TA_10_8_4() {
+        AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+        thisAccountWillBeDeletedAfterTests(developer);
 
+        String branch = "Working";
+        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
 
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+        ACCObject acc, acc_association, accForBase;
+        ASCCObject ascc;
+        ASCCPObject asccp;
+        BCCPObject bccp, bccp_to_append;
+
+        {
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            acc = coreComponentAPI.createRandomACC(developer, release, namespace, "Draft");
+            coreComponentAPI.updateACC(acc);
+
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Draft");
+            BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "Draft");
+            bcc.setCardinalityMax(1);
+            coreComponentAPI.updateBCC(bcc);
+
+            acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "Draft");
+            bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "Draft");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Draft");
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "Draft");
+            ascc = coreComponentAPI.appendASCC(acc, asccp, "Draft");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+        }
+        ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        WebElement accNode = accViewEditPage.getNodeByPath("/" + acc.getDen());
+        ACCViewEditPage.ACCPanel accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals("1", getText(accPanel.getRevisionField()));
+        assertEquals("Draft", getText(accPanel.getStateField()));
+        accViewEditPage.backToWIP();
+        accViewEditPage.hitUpdateButton();
+
+        viewEditCoreComponentPage.openPage();
+        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals("WIP", getText(accPanel.getStateField()));
+        WebElement asccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + asccp.getPropertyTerm());
+        ACCViewEditPage.ASCCPPanel asccpPanel = accViewEditPage.getASCCPanelContainer(asccNode).getASCCPPanel();
+        assertEquals("WIP", getText(asccpPanel.getStateField()));
+        WebElement bccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + bccp_to_append.getPropertyTerm());
+        ACCViewEditPage.BCCPPanel bccpPanel = accViewEditPage.getBCCPanelContainer(bccNode).getBCCPPanel();
+        assertEquals("WIP", getText(bccpPanel.getStateField()));
     }
 
 }
