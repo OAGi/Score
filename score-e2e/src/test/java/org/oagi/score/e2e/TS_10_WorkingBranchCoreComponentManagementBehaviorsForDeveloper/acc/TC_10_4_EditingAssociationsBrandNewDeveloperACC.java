9f5e05cfde5cbc7a6619afd00bcfef19590c491c
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-03-31 15:11:57 -0400

    update TA_10_4_22

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_4_EditingAssociationsBrandNewDeveloperACC.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_10_WorkingBranchCoreComponentManagementBehaviorsForDeveloper/acc/TC_10_4_EditingAssociationsBrandNewDeveloperACC.java
@@ -2831,7 +2831,6 @@ public class TC_10_4_EditingAssociationsBrandNewDeveloperACC extends BaseTest {
 
     @Test
     public void test_TA_10_4_22() {
-
         AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
         thisAccountWillBeDeletedAfterTests(developer);
         AppUserObject anotherDeveloper = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
@@ -2840,69 +2839,55 @@ public class TC_10_4_EditingAssociationsBrandNewDeveloperACC extends BaseTest {
         thisAccountWillBeDeletedAfterTests(endUser);
 
         String branch = "Working";
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+        ACCObject acc, acc_association, accForBase;
+        ASCCObject ascc;
+        ASCCPObject asccp;
+        BCCPObject bccp, bccp_to_append;
 
+        {
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            acc = coreComponentAPI.createRandomACC(developer, release, namespace, "WIP");
+            accForBase = coreComponentAPI.createRandomACC(developer, release, namespace, "WIP");
+            coreComponentAPI.updateACC(acc);
+
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "WIP");
+            BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "WIP");
+            bcc.setCardinalityMax(1);
+            coreComponentAPI.updateBCC(bcc);
+
+            BCCObject bcc2 = coreComponentAPI.appendBCC(accForBase, bccp, "WIP");
+            coreComponentAPI.updateBCC(bcc2);
+
+            acc_association = coreComponentAPI.createRandomACC(developer, release, namespace, "WIP");
+            bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, developer, namespace, "WIP");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "WIP");
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, developer, namespace, "WIP");
+            ascc = coreComponentAPI.appendASCC(acc, asccp, "WIP");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+        }
         HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
         ViewEditCoreComponentPage viewEditCoreComponentPage =
                 homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
         ACCViewEditPage accViewEditPage;
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByDenAndBranch("Document Identification Base. Details", branch);
-        accViewEditPage.hitReviseButton();
-        String url = getDriver().getCurrentUrl();
-        BigInteger baseACCManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
 
+        getAPIFactory().getCoreComponentAPI().updateBasedACC(acc, accForBase);
         viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByDenAndBranch("Manufacturing Route Operation Base. Details", branch);
-        url = getDriver().getCurrentUrl();
-        BigInteger refactorACCManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
-        accViewEditPage.hitReviseButton();
-        String nodePath = "/Manufacturing Route Operation Base. Details/Termination Indicator";
-        SelectBaseACCToRefactorDialog selectBaseACCToRefactorDialog = accViewEditPage.refactorToBaseACC(nodePath, "Termination Indicator");
+        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(acc.getAccManifestId());
+        String nodePath = "/" + acc.getDen() + "/" + bccp.getPropertyTerm();
+        SelectBaseACCToRefactorDialog selectBaseACCToRefactorDialog = accViewEditPage.refactorToBaseACC(nodePath, bccp.getPropertyTerm());
         WebElement tr;
-        tr = selectBaseACCToRefactorDialog.getTableRecordByValue("Document Identification Base");
+        tr = selectBaseACCToRefactorDialog.getTableRecordAtIndex(1);
         assertTrue(tr.isDisplayed());
         click(tr.findElement(By.className("mat-column-" + "select")));
         selectBaseACCToRefactorDialog.hitAnalyzeButton();
-        assertEnabled(selectBaseACCToRefactorDialog.getRefactorButton(true));
-        selectBaseACCToRefactorDialog.hitRefactorButton();
+        assertDisabled(selectBaseACCToRefactorDialog.getRefactorButton(false));
 
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(refactorACCManifestId);
-        WebElement movedBCCPNode = accViewEditPage.getNodeByPath(nodePath);
-        assertFalse(movedBCCPNode.isDisplayed());
-
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(baseACCManifestId);
-        movedBCCPNode = accViewEditPage.getNodeByPath("/Document Identification Base. Details/Termination Indicator");
-        assertTrue(movedBCCPNode.isDisplayed());
-
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(refactorACCManifestId);
-        accViewEditPage.hitCancelButton();
-        movedBCCPNode = accViewEditPage.getNodeByPath(nodePath);
-        assertTrue(movedBCCPNode.isDisplayed());
-
-        //Refactor a second time
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(refactorACCManifestId);
-        accViewEditPage.hitReviseButton();
-        WebElement bccNode = accViewEditPage.getNodeByPath(nodePath);
-        ACCViewEditPage.BCCPPanel bccpPanel = accViewEditPage.getBCCPanelContainer(bccNode).getBCCPPanel();
-        bccpPanel.setDefinition("refactoring2");
-
-        selectBaseACCToRefactorDialog = accViewEditPage.refactorToBaseACC(nodePath, "Termination Indicator");
-        tr = selectBaseACCToRefactorDialog.getTableRecordByValue("Document Identification Base");
-        assertTrue(tr.isDisplayed());
-        click(tr.findElement(By.className("mat-column-" + "select")));
-        selectBaseACCToRefactorDialog.hitAnalyzeButton();
-        assertEnabled(selectBaseACCToRefactorDialog.getRefactorButton(true));
-        selectBaseACCToRefactorDialog.hitRefactorButton();
-
-        viewEditCoreComponentPage.openPage();
-        accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(baseACCManifestId);
-        bccNode = accViewEditPage.getNodeByPath("/Document Identification Base. Details/Termination Indicator");
-        bccpPanel = accViewEditPage.getBCCPanelContainer(bccNode).getBCCPPanel();
-        assertEquals("refactoring2", getText(bccpPanel.getDefinitionField()));
     }
 
     @Test
