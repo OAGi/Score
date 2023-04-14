dd4c7489ce11c9443b4675b6be1f6e73ea71a154
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-04-14 15:30:25 -0400

    Implementation of the TC_17_4_TA_7 assertion

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_17_ReleaseBranchCodeListManagementForEndUser/TC_17_4_AmendAnEndUserCodeList.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_17_ReleaseBranchCodeListManagementForEndUser/TC_17_4_AmendAnEndUserCodeList.java
@@ -389,6 +389,48 @@ public class TC_17_4_AmendAnEndUserCodeList extends BaseTest {
             editCodeListPage.hitUpdateButton();
         }
     }
+    @Test
+    @DisplayName("TC_17_4_TA_7")
+    public void test_TA_7() {
+        AppUserObject endUserA;
+        ReleaseObject branch;
+        ArrayList<CodeListObject> codeListForTesting = new ArrayList<>();
+        {
+            endUserA = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+            thisAccountWillBeDeletedAfterTests(endUserA);
+
+            AppUserObject endUserB = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+            thisAccountWillBeDeletedAfterTests(endUserB);
+
+            branch = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("10.8.5");
+            NamespaceObject namespaceEUB = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUserB);
+
+            /**
+             * Create Production end-user Code List for a particular release branch.
+             */
+            CodeListObject codeList = getAPIFactory().getCodeListAPI().
+                    createRandomCodeList(endUserB, namespaceEUB, branch, "Production");
+            getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeList, endUserB);
+            codeListForTesting.add(codeList);
+        }
+        HomePage homePage = loginPage().signIn(endUserA.getLoginId(), endUserA.getPassword());
+
+        for (CodeListObject cl : codeListForTesting){
+            ViewEditCodeListPage viewEditCodeListPage = homePage.getCoreComponentMenu().openViewEditCodeListSubMenu();
+            EditCodeListPage editCodeListPage = viewEditCodeListPage.openCodeListViewEditPageByNameAndBranch(cl.getName(), branch.getReleaseNumber());
+            int previousRevisionNumber = Integer.parseInt(getText(editCodeListPage.getRevisionField()));
+            editCodeListPage.hitAmendButton();
+            assertTrue(getText(editCodeListPage.getStateField()).equals("WIP"));
+            assertEquals(previousRevisionNumber+1,Integer.parseInt(getText(editCodeListPage.getRevisionField())));
+            EditCodeListValueDialog editCodeListValueDialog = editCodeListPage.addCodeListValue();
+            String newValueCode = "new value code";
+            editCodeListValueDialog.setCode(newValueCode);
+            editCodeListValueDialog.setMeaning("new value meaning");
+            editCodeListValueDialog.hitAddButton();
+            editCodeListPage.selectCodeListValue(newValueCode);
+            editCodeListPage.removeCodeListValue();
+        }
+    }
 
     @AfterEach
     public void tearDown() {
