54a053d1b41709b061b07f49b7ea8557cbf1b75e
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-04-17 14:10:35 -0400

    Implementation of the TC_17_7 assertions

--- score-e2e/src/test/java/org/oagi/score/e2e/suite/TS_17.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/suite/TS_17.java
@@ -12,7 +12,9 @@ import org.oagi.score.e2e.TS_17_ReleaseBranchCodeListManagementForEndUser.*;
         TC_17_2_CreatingABrandNewEndUserCodeList.class,
         TC_17_3_EditingABrandNewEndUserCodeList.class,
         TC_17_4_AmendAnEndUserCodeList.class,
-        TC_17_5_EndUserCodeListStateManagement.class
+        TC_17_5_EndUserCodeListStateManagement.class,
+        TC_17_6_DeletingACodeList.class,
+        TC_17_7_RestoringEndUserCodeList.class
 })
 public class TS_17 {
 }
