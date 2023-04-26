57d234c5b1dfdb386636ea9d6594937a10be1c89
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-04-26 14:33:40 -0400

    add TA_15_1_2

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/TC_15_1_AccessCoreComponentViewingEditingCommenting.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/TC_15_1_AccessCoreComponentViewingEditingCommenting.java
@@ -10,8 +10,10 @@ import org.oagi.score.e2e.api.CoreComponentAPI;
 import org.oagi.score.e2e.menu.CoreComponentMenu;
 import org.oagi.score.e2e.obj.*;
 import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
 import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +22,9 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.oagi.score.e2e.AssertionHelper.assertDisabled;
+import static org.oagi.score.e2e.AssertionHelper.assertEnabled;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_15_1_AccessCoreComponentViewingEditingCommenting extends BaseTest {
@@ -184,7 +189,79 @@ public class TC_15_1_AccessCoreComponentViewingEditingCommenting extends BaseTes
 
     @Test
     public void test_TA_15_1_2() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUser);
+
+        ASCCPObject asccp;
+        BCCPObject bccp;
+        ACCObject acc;
+
+        {
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            acc = coreComponentAPI.createRandomACC(endUser, release, namespace, "WIP");
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, endUser, namespace, "WIP");
+            BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "WIP");
+            bcc.setCardinalityMax(1);
+            coreComponentAPI.updateBCC(bcc);
+
+            ACCObject acc_association = coreComponentAPI.createRandomACC(endUser, release, namespace, "WIP");
+            BCCPObject bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, endUser, namespace, "WIP");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "WIP");
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, endUser, namespace, "WIP");
+            ASCCObject ascc = coreComponentAPI.appendASCC(acc, asccp, "WIP");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+        }
+
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        CoreComponentMenu coreComponentMenu = homePage.getCoreComponentMenu();
+        ViewEditCoreComponentPage viewEditCoreComponentPage = coreComponentMenu.openViewEditCoreComponentSubMenu();
+        ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByDenAndBranch(acc.getDen(), release.getReleaseNumber());
+        WebElement accNode = accViewEditPage.getNodeByPath("/" + acc.getDen());
+        ACCViewEditPage.ACCPanel accPanel = accViewEditPage.getACCPanel(accNode);
+        /**
+         * developer can edit the details of a CC that is in WIP state and owned by him
+         */
+
+        assertEquals("WIP", getText(accViewEditPage.getStateField()));
+        assertDisabled(accViewEditPage.getStateField());
+        assertDisabled(accViewEditPage.getGUIDField());
+        assertDisabled(accViewEditPage.getDENField());
+        assertEnabled(accViewEditPage.getObjectClassTermField());
+        assertEnabled(accViewEditPage.getDefinitionField());
+        assertEnabled(accViewEditPage.getDefinitionSourceField());
+        assertEnabled(accViewEditPage.getNamespaceField());
+        assertEnabled(accPanel.getComponentTypeSelectField());
+
+        accViewEditPage.openPage(); // refresh the page to erase the snackbar message
+        WebElement bccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + bccp.getPropertyTerm());
+        ACCViewEditPage.BCCPanelContainer bccPanelContainer = accViewEditPage.getBCCPanelContainer(bccNode);
+        assertEquals("WIP", getText(bccPanelContainer.getBCCPanel().getStateField()));
+        assertDisabled(bccPanelContainer.getBCCPPanel().getStateField());
+        assertDisabled(bccPanelContainer.getBCCPanel().getGUIDField());
+        assertDisabled(bccPanelContainer.getBCCPanel().getDENField());
+        assertDisabled(bccPanelContainer.getBCCPanel().getValueConstraintSelectField());
+        assertEnabled(bccPanelContainer.getBCCPanel().getDefinitionField());
+        assertEnabled(bccPanelContainer.getBCCPanel().getDefinitionSourceField());
+
+        accViewEditPage.openPage();
+        WebElement asccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + asccp.getPropertyTerm());
+        ACCViewEditPage.ASCCPanelContainer asccPanelContainer = accViewEditPage.getASCCPanelContainer(asccNode);
+        asccNode = accViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + asccp.getPropertyTerm());
+        asccPanelContainer = accViewEditPage.getASCCPanelContainer(asccNode);
+
+        assertEquals("WIP", getText(asccPanelContainer.getASCCPanel().getStateField()));
+        assertDisabled(asccPanelContainer.getASCCPanel().getStateField());
+        assertDisabled(asccPanelContainer.getASCCPanel().getGUIDField());
+        assertDisabled(asccPanelContainer.getASCCPanel().getDENField());
+        assertEnabled(asccPanelContainer.getASCCPanel().getDefinitionField());
+        assertEnabled(asccPanelContainer.getASCCPanel().getDefinitionSourceField());
     }
 
     @Test
