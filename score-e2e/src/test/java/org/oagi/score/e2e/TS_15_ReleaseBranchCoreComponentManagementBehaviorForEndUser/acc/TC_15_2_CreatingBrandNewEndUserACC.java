b1e4bc4a07b2b671a8c99d6bc62d56981bda3d30
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-04-28 15:17:16 -0400

    add TA_15_2_2

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/acc/TC_15_2_CreatingBrandNewEndUserACC.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/acc/TC_15_2_CreatingBrandNewEndUserACC.java
@@ -6,15 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.oagi.score.e2e.BaseTest;
+import org.oagi.score.e2e.obj.ACCObject;
+import org.oagi.score.e2e.obj.ASCCPObject;
 import org.oagi.score.e2e.obj.AppUserObject;
 import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
 import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
 import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.oagi.score.e2e.AssertionHelper.assertNotChecked;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_15_2_CreatingBrandNewEndUserACC extends BaseTest {
@@ -55,9 +63,26 @@ public class TC_15_2_CreatingBrandNewEndUserACC extends BaseTest {
 
     @Test
     public void test_TA_15_2_2() {
+        AppUserObject endUser= getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
-
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.createACC(branch);
+        String url = getDriver().getCurrentUrl();
+        int idx = url.lastIndexOf("/");
+        BigInteger manifestId = new BigInteger(url.substring(idx + 1));
+        ACCObject acc = getAPIFactory().getCoreComponentAPI().getACCByManifestId(manifestId);
+        WebElement accNode =  accViewEditPage.getNodeByPath("/" + acc.getDen());
+        ACCViewEditPage.ACCPanel accPanel = accViewEditPage.getACCPanel(accNode);
+        assertEquals("Object Class Term", getText(accPanel.getObjectClassTermField()));
+        assertEquals("Object Class Term. Details", getText(accPanel.getDENField()));
+        assertEquals("Semantics", getText(accPanel.getComponentTypeSelectField()));
+        assertNotChecked(accPanel.getAbstractCheckbox());
+        assertNotChecked(accPanel.getDeprecatedCheckbox());
+        assertEquals("", getText(accPanel.getDefinitionSourceField()));
+        assertEquals("", getText(accPanel.getDefinitionField()));
     }
-
-
 }
