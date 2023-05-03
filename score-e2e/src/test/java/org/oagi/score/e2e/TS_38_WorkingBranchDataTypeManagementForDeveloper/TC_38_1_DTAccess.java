1cda18e008a8afa9985f28ce76f21e00abb49f8e
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-05-03 16:09:50 -0400

    Implementation of the TC_38_1_TA_1 assertion

---
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_38_WorkingBranchDataTypeManagementForDeveloper/TC_38_1_DTAccess.java
@@ -0,0 +1,83 @@
+package org.oagi.score.e2e.TS_38_WorkingBranchDataTypeManagementForDeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.oagi.score.e2e.BaseTest;
+import org.oagi.score.e2e.obj.*;
+import org.oagi.score.e2e.page.HomePage;
+
+import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class TC_38_1_DTAccess extends BaseTest {
+
+    private final List<AppUserObject> randomAccounts = new ArrayList<>();
+
+    @BeforeEach
+    public void init() {
+        super.init();
+
+    }
+
+    private void thisAccountWillBeDeletedAfterTests(AppUserObject appUser) {
+        this.randomAccounts.add(appUser);
+    }
+
+    @Test
+    @DisplayName("TC_38_1_TA_1")
+    public void test_TA_1() {
+        AppUserObject developerA;
+        ReleaseObject branch;
+        ArrayList<DTObject> dtForTesting = new ArrayList<>();
+        {
+            developerA = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+            thisAccountWillBeDeletedAfterTests(developerA);
+
+            AppUserObject developerB = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+            thisAccountWillBeDeletedAfterTests(developerB);
+
+            branch = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+            NamespaceObject namespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+
+            DTObject baseDT = getAPIFactory().getCoreComponentAPI().getCDTByDENAndReleaseNum("Numeric. Type", "Working");
+            DTObject dtWIP = getAPIFactory().getCoreComponentAPI().createRandomBDT(baseDT, developerB, namespace, "WIP");
+            dtForTesting.add(dtWIP);
+
+            DTObject dtDraft = getAPIFactory().getCoreComponentAPI().createRandomBDT(baseDT, developerB, namespace, "Draft");
+            dtForTesting.add(dtDraft);
+
+            DTObject dtCandidate = getAPIFactory().getCoreComponentAPI().createRandomBDT(baseDT, developerB, namespace, "Candidate");
+            dtForTesting.add(dtCandidate);
+        }
+
+        HomePage homePage = loginPage().signIn(developerA.getLoginId(), developerA.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage = homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        viewEditCoreComponentPage.setBranch(branch.getReleaseNumber());
+        for (DTObject dt : dtForTesting) {
+            assertFalse(dt.getOwnerUserId().equals(developerA.getAppUserId()));
+            viewEditCoreComponentPage.setDEN(dt.getDen());
+            viewEditCoreComponentPage.hitSearchButton();
+            assertDoesNotThrow(() -> viewEditCoreComponentPage.getTableRecordByValue(dt.getDen()));
+        }
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        super.tearDown();
+        // Delete random accounts
+        this.randomAccounts.forEach(newUser -> {
+            getAPIFactory().getAppUserAPI().deleteAppUserByLoginId(newUser.getLoginId());
+        });
+    }
+}
