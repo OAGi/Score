0d576406c78ae840b96b8964748cb0a5b09ad848
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-05-03 14:22:14 -0400

    add TC_15_12_1

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/asccp/TC_15_12_AmendEndUserASCCP.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/asccp/TC_15_12_AmendEndUserASCCP.java
@@ -1,4 +1,176 @@
 package org.oagi.score.e2e.TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser.asccp;
 
-public class TC_15_12_AmendEndUserASCCP {
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.oagi.score.e2e.BaseTest;
+import org.oagi.score.e2e.api.CoreComponentAPI;
+import org.oagi.score.e2e.menu.CoreComponentMenu;
+import org.oagi.score.e2e.obj.*;
+import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
+import org.oagi.score.e2e.page.core_component.ASCCPViewEditPage;
+import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.oagi.score.e2e.AssertionHelper.*;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class TC_15_12_AmendEndUserASCCP extends BaseTest {
+    private List<AppUserObject> randomAccounts = new ArrayList<>();
+
+    @BeforeEach
+    public void init() {
+        super.init();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        super.tearDown();
+
+        // Delete random accounts
+        this.randomAccounts.forEach(randomAccount -> {
+            getAPIFactory().getAppUserAPI().deleteAppUserByLoginId(randomAccount.getLoginId());
+        });
+    }
+
+    private void thisAccountWillBeDeletedAfterTests(AppUserObject appUser) {
+        this.randomAccounts.add(appUser);
+    }
+
+    @Test
+    public void test_TA_15_12_1() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        AppUserObject anotherUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(anotherUser);
+
+        String branch = "10.8.7.1";
+
+        ReleaseObject release = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber(branch);
+        NamespaceObject namespace_endUser = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUser);
+
+        ASCCPObject asccp;
+        BCCPObject bccp;
+        ACCObject acc;
+
+        {
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            acc = coreComponentAPI.createRandomACC(anotherUser, release, namespace_endUser, "Production");
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", release.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, anotherUser, namespace_endUser, "Production");
+            BCCObject bcc = coreComponentAPI.appendBCC(acc, bccp, "Production");
+            bcc.setCardinalityMax(1);
+            coreComponentAPI.updateBCC(bcc);
+
+            ACCObject acc_association = coreComponentAPI.createRandomACC(anotherUser, release, namespace_endUser, "Production");
+            BCCPObject bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, anotherUser, namespace_endUser, "Production");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Production");
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, anotherUser, namespace_endUser, "Production");
+            ASCCObject ascc = coreComponentAPI.appendASCC(acc, asccp, "Production");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+        }
+
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        CoreComponentMenu coreComponentMenu = homePage.getCoreComponentMenu();
+        ViewEditCoreComponentPage viewEditCoreComponentPage = coreComponentMenu.openViewEditCoreComponentSubMenu();
+        ASCCPViewEditPage asccpViewEditPage = viewEditCoreComponentPage.openASCCPViewEditPageByDenAndBranch(asccp.getDen(), branch);
+
+        asccpViewEditPage.hitAmendButton();
+
+        //reload the page to verify
+        viewEditCoreComponentPage.openPage();
+        asccpViewEditPage = viewEditCoreComponentPage.openASCCPViewEditPageByManifestID(asccp.getAsccpManifestId());
+        WebElement asccNode = asccpViewEditPage.getNodeByPath("/" + acc.getDen() + "/" + asccp.getPropertyTerm());
+        ASCCPViewEditPage.ASCCPPanel asccpPanel = asccpViewEditPage.getASCCPanelContainer(asccNode).getASCCPPanel();
+        assertEquals("2", getText(asccpPanel.getRevisionField()));
+        assertEquals("WIP", getText(asccpPanel.getStateField()));
+        assertEquals(asccp.getPropertyTerm(), getText(asccpPanel.getPropertyTermField()));
+        assertEquals(asccp.getDen(), getText(asccpPanel.getDENField()));
+
+        assertChecked(asccpPanel.getReusableCheckbox());
+        assertEnabled(asccpPanel.getReusableCheckbox());
+
+        assertNotChecked(asccpPanel.getNillableCheckbox());
+        assertEnabled(asccpPanel.getNillableCheckbox());
+
+        assertNotChecked(asccpPanel.getDeprecatedCheckbox());
+        assertEnabled(asccpPanel.getDeprecatedCheckbox());
+
+        assertEquals(asccp.getDefinitionSource(), getText(asccpPanel.getDefinitionSourceField()));
+        assertEquals(asccp.getDefinition(), getText(asccpPanel.getDefinitionField()));
+    }
+
+    @Test
+    public void test_TA_15_12_2() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_3() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_a() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_b() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_c() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_d() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_e() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_f() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_g() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_4_h() {
+
+    }
+
+    @Test
+    public void test_TA_15_12_5() {
+
+    }
+
+
+    @Test
+    public void test_TA_15_12_6() {
+
+    }
 }
