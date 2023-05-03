6453bfb75bd807ea817d5f2dd9e93886c53792e0
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-05-03 16:46:22 -0400

    add TC_15_15_1

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/bccp/TC_15_15_EditingBrandNewEndUserBCCP.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_15_ReleaseBranchCoreComponentManagementBehaviorForEndUser/bccp/TC_15_15_EditingBrandNewEndUserBCCP.java
@@ -7,18 +7,24 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.oagi.score.e2e.BaseTest;
 import org.oagi.score.e2e.obj.AppUserObject;
+import org.oagi.score.e2e.obj.NamespaceObject;
 import org.oagi.score.e2e.page.HomePage;
 import org.oagi.score.e2e.page.core_component.BCCPCreateDialog;
 import org.oagi.score.e2e.page.core_component.BCCPViewEditPage;
 import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.commons.lang3.RandomStringUtils.randomPrint;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.oagi.score.e2e.AssertionHelper.assertEnabled;
+import static org.jooq.tools.StringUtils.isEmpty;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.oagi.score.e2e.AssertionHelper.*;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
+import static org.oagi.score.e2e.impl.PageHelper.visibilityOfElementLocated;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_15_15_EditingBrandNewEndUserBCCP extends BaseTest {
@@ -77,41 +83,201 @@ public class TC_15_15_EditingBrandNewEndUserBCCP extends BaseTest {
 
     @Test
     public void test_TA_15_15_1_b() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
 
+        String randomPropertyTerm = randomAlphabetic(5, 10).replaceAll(" ", "");
+        randomPropertyTerm = Character.toUpperCase(randomPropertyTerm.charAt(0)) + randomPropertyTerm.substring(1).toLowerCase();
+        bccpPanel.setPropertyTerm("Test Object " + randomPropertyTerm);
+
+        String denText = getText(bccpPanel.getDENField());
+        assertEquals("Test Object " + randomPropertyTerm + ". Code", denText);
     }
 
     @Test
     public void test_TA_15_15_1_c() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
+
+        String randomPropertyTerm = randomAlphabetic(5, 10).replaceAll(" ", "");
+        randomPropertyTerm = Character.toUpperCase(randomPropertyTerm.charAt(0)) + randomPropertyTerm.substring(1).toLowerCase();
+        bccpPanel.setPropertyTerm("Test Object " + randomPropertyTerm);
+        bccpPanel.toggleNillable();
+        NamespaceObject namespace = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUser);
+        bccpPanel.setNamespace(namespace.getUri());
+
+        assertThrows(TimeoutException.class, () -> bccpViewEditPage.hitUpdateButton());
+        assertEquals("Update without definitions.", getText(visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-dialog-container//div[contains(@class, \"header\")]"))));
 
     }
 
     @Test
     public void test_TA_15_15_1_d() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
+
+        assertFalse(bccpPanel.getGUIDField().isEnabled());
+        assertFalse(bccpPanel.getDENField().isEnabled());
+        assertDisabled(bccpPanel.getDeprecatedCheckbox());
     }
 
     @Test
     public void test_TA_15_15_1_e() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.DTPanel dtPanel = bccpViewEditPage.getBCCPPanelContainer().getDTPanel();
+
+        assertFalse(dtPanel.getCoreComponentField().isEnabled());
+        assertEquals("DT", getText(dtPanel.getCoreComponentField()));
+        assertFalse(dtPanel.getReleaseField().isEnabled());
+        assertEquals(getText(bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel().getReleaseField()),
+                getText(dtPanel.getReleaseField()));
+        assertFalse(dtPanel.getRevisionField().isEnabled());
+        assertFalse(dtPanel.getStateField().isEnabled());
+        assertEquals("Published", getText(dtPanel.getStateField()));
+        assertFalse(dtPanel.getOwnerField().isEnabled());
+        assertFalse(dtPanel.getGUIDField().isEnabled());
+        assertFalse(dtPanel.getDENField().isEnabled());
+        assertEquals("System Environment_ Code. Type", getText(dtPanel.getDENField()));
+        assertFalse(dtPanel.getDataTypeTermField().isEnabled());
+        assertEquals("Code", getText(dtPanel.getDataTypeTermField()));
+        assertFalse(dtPanel.getQualifierField().isEnabled());
+        assertEquals("System Environment", getText(dtPanel.getQualifierField()));
+        assertFalse(dtPanel.getDefinitionSourceField().isEnabled());
+        assertFalse(dtPanel.getDefinitionField().isEnabled());
     }
     @Test
     public void test_TA_15_15_1_f() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
 
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
+
+        bccpPanel.setValueConstraint("Fixed Value");
+        String fixedValue = randomAlphabetic(5, 10);
+        bccpPanel.setFixedValue(fixedValue);
+
+        bccpPanel.setValueConstraint("Default Value");
+        String defaultValue = randomAlphabetic(5, 10);
+        bccpPanel.setDefaultValue(defaultValue);
+
+        bccpPanel.setValueConstraint("Fixed Value");
+        assertTrue(isEmpty(getText(bccpPanel.getFixedValueField())));
+
+        bccpPanel.setValueConstraint("Default Value");
+        assertTrue(isEmpty(getText(bccpPanel.getDefaultValueField())));
     }
 
     @Test
     public void test_TA_15_15_1_g() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
+
+        bccpPanel.setValueConstraint("Fixed Value");
+        String fixedValue = randomAlphabetic(5, 10);
+        bccpPanel.setFixedValue(fixedValue);
+
+        bccpPanel.toggleNillable();
+
+        String valueConstraintSelectText = getText(bccpPanel.getValueConstraintSelectField());
+        assertEquals("None", valueConstraintSelectText);
+
+        bccpPanel.setValueConstraint("Fixed Value");
+        bccpPanel.setFixedValue(fixedValue);
+        assertNotChecked(bccpPanel.getNillableCheckbox());
 
     }
 
     @Test
     public void test_TA_15_15_1_h() {
+        AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(endUser);
+
+        String branch = "10.8.7.1";
+        HomePage homePage = loginPage().signIn(endUser.getLoginId(), endUser.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+        BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+        BCCPViewEditPage.BCCPPanel bccpPanel = bccpViewEditPage.getBCCPPanelContainer().getBCCPPanel();
+
+        assertTrue(bccpPanel.getPropertyTermField().isEnabled());
+        assertEnabled(bccpPanel.getNillableCheckbox());
+        assertTrue(bccpPanel.getValueConstraintSelectField().isEnabled());
+        assertTrue(bccpPanel.getNamespaceSelectField().isEnabled());
+        assertTrue(bccpPanel.getDefinitionField().isEnabled());
+        assertTrue(bccpPanel.getDefinitionSourceField().isEnabled());
+
+        String randomPropertyTerm = randomAlphabetic(5, 10).replaceAll(" ", "");
+        randomPropertyTerm = Character.toUpperCase(randomPropertyTerm.charAt(0)) + randomPropertyTerm.substring(1).toLowerCase();
+        bccpPanel.setPropertyTerm("Test Object " + randomPropertyTerm);
+        bccpPanel.toggleNillable();
+        String namespace = "http://www.openapplications.org/oagis/10";
+        bccpPanel.setNamespace(namespace);
+        String definition = randomPrint(50, 100).trim();
+        bccpPanel.setDefinition(definition);
+
+        assertTrue(bccpViewEditPage.getUpdateButton(true).isEnabled());
+
+        NamespaceObject developerNamespace = getAPIFactory().getNamespaceAPI().getNamespaceByURI("http://www.openapplications.org/oagis/10");
+
+        AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+        thisAccountWillBeDeletedAfterTests(developer);
+
+        assertThrows(TimeoutException.class, () -> bccpPanel.setNamespace(developerNamespace.getUri()));
 
     }
 
     @Test
     public void test_TA_15_15_2() {
+
+
 
     }
 
