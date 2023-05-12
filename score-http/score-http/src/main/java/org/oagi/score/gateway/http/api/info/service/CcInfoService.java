512360ba60c3599d38c94836e6bca5b3135aea15
Hakju Oh <hakju.oh@gmail.com>
2023-05-12 13:56:02 -0400

    Issue #1494
    Add 'Core Components' tab to the homepage.

--- score-http/score-http/src/main/java/org/oagi/score/gateway/http/api/info/service/CcInfoService.java
+++ score-http/score-http/src/main/java/org/oagi/score/gateway/http/api/info/service/CcInfoService.java
@@ -1,18 +1,26 @@
 package org.oagi.score.gateway.http.api.info.service;
 
+import org.oagi.score.data.Release;
 import org.oagi.score.gateway.http.api.DataAccessForbiddenException;
+import org.oagi.score.gateway.http.api.bie_management.data.BieListRequest;
+import org.oagi.score.gateway.http.api.cc_management.data.CcListRequest;
+import org.oagi.score.gateway.http.api.cc_management.data.CcListTypes;
 import org.oagi.score.gateway.http.api.cc_management.service.CcListService;
-import org.oagi.score.gateway.http.api.info.data.SummaryCcExt;
-import org.oagi.score.gateway.http.api.info.data.SummaryCcExtInfo;
+import org.oagi.score.gateway.http.api.info.data.*;
+import org.oagi.score.gateway.http.api.release_management.service.ReleaseService;
 import org.oagi.score.gateway.http.configuration.security.SessionService;
 import org.oagi.score.repo.CoreComponentRepository;
+import org.oagi.score.repo.api.bie.model.BieState;
+import org.oagi.score.repo.component.release.ReleaseRepository;
 import org.oagi.score.service.common.data.AppUser;
 import org.oagi.score.service.common.data.CcState;
+import org.oagi.score.service.common.data.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,10 +34,56 @@ public class CcInfoService {
     private CoreComponentRepository ccRepository;
 
     @Autowired
+    private ReleaseRepository releaseRepository;
+
+    @Autowired
     private CcListService ccListService;
 
     @Autowired
     private SessionService sessionService;
+
+    public SummaryCcInfo getSummaryCcInfo(AuthenticatedPrincipal user) {
+        AppUser requester = sessionService.getAppUserByUsername(user);
+        if (user == null || requester == null) {
+            throw new DataAccessForbiddenException("Need authentication to access information.");
+        }
+
+        List<SummaryCc> summaryCcList = ccRepository.getSummaryCcList(requester);
+
+        SummaryCcInfo info = new SummaryCcInfo();
+        Map<CcState, Integer> numberOfTotalCcByStates =
+                summaryCcList.stream().collect(Collectors.toMap(SummaryCc::getState, (e) -> 1, Integer::sum));
+        info.setNumberOfTotalCcByStates(numberOfTotalCcByStates);
+
+        Map<CcState, Integer> numberOfMyCcByStates =
+                summaryCcList.stream()
+                        .filter(e -> e.getOwnerUserId().equals(requester.getAppUserId()))
+                        .collect(Collectors.toMap(SummaryCc::getState, (e) -> 1, Integer::sum));
+        info.setNumberOfMyCcByStates(numberOfMyCcByStates);
+
+        Map<String, Map<CcState, Integer>> ccByUsersAndStates = summaryCcList.stream()
+                .collect(groupingBy(SummaryCc::getOwnerUsername,
+                        Collectors.toMap(SummaryCc::getState, (e) -> 1, Integer::sum)));
+        info.setCcByUsersAndStates(ccByUsersAndStates);
+
+        CcListRequest request = new CcListRequest();
+        request.setTypes(CcListTypes.fromString("ACC,ASCCP,BCCP,BDT"));
+        request.setStates(Arrays.asList(CcState.WIP, CcState.Draft, CcState.Candidate));
+        PageRequest pageRequest = new PageRequest();
+        pageRequest.setSortActive("lastUpdateTimestamp");
+        pageRequest.setSortDirection("desc");
+        pageRequest.setPageIndex(0);
+        pageRequest.setPageSize(5);
+        request.setOwnerLoginIds(Arrays.asList(String.valueOf(requester.getLoginId())));
+        request.setPageRequest(pageRequest);
+
+        Release workingRelease = releaseRepository.findByReleaseNum("Working").stream().findFirst().get();
+        request.setReleaseId(workingRelease.getReleaseId());
+
+        info.setMyRecentCCs(ccListService.getCcList(request).getList());
+
+        return info;
+    }
 
     public SummaryCcExtInfo getSummaryCcExtInfo(AuthenticatedPrincipal user, BigInteger releaseId) {
         AppUser requester = sessionService.getAppUserByUsername(user);
--- score-http/score-http/src/main/java/org/oagi/score/repo/CoreComponentRepository.java
+++ score-http/score-http/src/main/java/org/oagi/score/repo/CoreComponentRepository.java
@@ -1,11 +1,14 @@
 package org.oagi.score.repo;
 
+import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.types.ULong;
 import org.oagi.score.gateway.http.api.cc_management.data.node.CcBccpNode;
+import org.oagi.score.gateway.http.api.info.data.SummaryCc;
 import org.oagi.score.gateway.http.api.info.data.SummaryCcExt;
 import org.oagi.score.gateway.http.configuration.security.SessionService;
 import org.oagi.score.repo.api.impl.jooq.entity.tables.records.*;
+import org.oagi.score.service.common.data.AppUser;
 import org.oagi.score.service.common.data.CcState;
 import org.oagi.score.service.common.data.OagisComponentType;
 import org.oagi.score.service.log.LogRepository;
@@ -14,11 +17,11 @@ import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.jooq.impl.DSL.and;
-import static org.jooq.impl.DSL.max;
+import static org.jooq.impl.DSL.*;
 import static org.oagi.score.repo.api.impl.jooq.entity.Tables.*;
 import static org.oagi.score.repo.api.impl.jooq.entity.tables.Acc.ACC;
 import static org.oagi.score.repo.api.impl.jooq.entity.tables.Ascc.ASCC;
@@ -222,6 +225,60 @@ public class CoreComponentRepository {
                 .from(BCCP)
                 .where(BCCP.BCCP_ID.eq(ULong.valueOf(bccpId)))
                 .fetchOneInto(CcBccpNode.class);
+    }
+
+    public List<SummaryCc> getSummaryCcList(AppUser requester) {
+        List<SummaryCc> unionOfSummaryCcList = dslContext.select(ACC_MANIFEST.ACC_MANIFEST_ID.as("manifestId"),
+                        inline("ACC").as("type"),
+                        ACC.LAST_UPDATE_TIMESTAMP,
+                        ACC.STATE,
+                        ACC.OWNER_USER_ID,
+                        APP_USER.LOGIN_ID.as("ownerUsername"),
+                        ACC.DEN)
+                .from(ACC_MANIFEST)
+                .join(ACC).on(ACC_MANIFEST.ACC_ID.eq(ACC.ACC_ID))
+                .join(RELEASE).on(ACC_MANIFEST.RELEASE_ID.eq(RELEASE.RELEASE_ID))
+                .join(APP_USER).on(ACC.OWNER_USER_ID.eq(APP_USER.APP_USER_ID))
+                .where(and(RELEASE.RELEASE_NUM.eq("Working"), ACC.STATE.in("WIP", "Draft", "Candidate")))
+                .union(dslContext.select(ASCCP_MANIFEST.ASCCP_MANIFEST_ID.as("manifestId"),
+                                inline("ASCCP").as("type"),
+                                ASCCP.LAST_UPDATE_TIMESTAMP,
+                                ASCCP.STATE,
+                                ASCCP.OWNER_USER_ID,
+                                APP_USER.LOGIN_ID.as("ownerUsername"),
+                                ASCCP.DEN)
+                        .from(ASCCP_MANIFEST)
+                        .join(ASCCP).on(ASCCP_MANIFEST.ASCCP_ID.eq(ASCCP.ASCCP_ID))
+                        .join(RELEASE).on(ASCCP_MANIFEST.RELEASE_ID.eq(RELEASE.RELEASE_ID))
+                        .join(APP_USER).on(ASCCP.OWNER_USER_ID.eq(APP_USER.APP_USER_ID))
+                        .where(and(RELEASE.RELEASE_NUM.eq("Working"), ASCCP.STATE.in("WIP", "Draft", "Candidate"))))
+                .union(dslContext.select(BCCP_MANIFEST.BCCP_MANIFEST_ID.as("manifestId"),
+                                inline("BCCP").as("type"),
+                                BCCP.LAST_UPDATE_TIMESTAMP,
+                                BCCP.STATE,
+                                BCCP.OWNER_USER_ID,
+                                APP_USER.LOGIN_ID.as("ownerUsername"),
+                                BCCP.DEN)
+                        .from(BCCP_MANIFEST)
+                        .join(BCCP).on(BCCP_MANIFEST.BCCP_ID.eq(BCCP.BCCP_ID))
+                        .join(RELEASE).on(BCCP_MANIFEST.RELEASE_ID.eq(RELEASE.RELEASE_ID))
+                        .join(APP_USER).on(BCCP.OWNER_USER_ID.eq(APP_USER.APP_USER_ID))
+                        .where(and(RELEASE.RELEASE_NUM.eq("Working"), BCCP.STATE.in("WIP", "Draft", "Candidate"))))
+                .union(dslContext.select(DT_MANIFEST.DT_MANIFEST_ID.as("manifestId"),
+                                inline("BDT").as("type"),
+                                DT.LAST_UPDATE_TIMESTAMP,
+                                DT.STATE,
+                                DT.OWNER_USER_ID,
+                                APP_USER.LOGIN_ID.as("ownerUsername"),
+                                DT.DEN)
+                        .from(DT_MANIFEST)
+                        .join(DT).on(and(DT_MANIFEST.DT_ID.eq(DT.DT_ID), DT_MANIFEST.BASED_DT_MANIFEST_ID.isNotNull()))
+                        .join(RELEASE).on(DT_MANIFEST.RELEASE_ID.eq(RELEASE.RELEASE_ID))
+                        .join(APP_USER).on(DT.OWNER_USER_ID.eq(APP_USER.APP_USER_ID))
+                        .where(and(RELEASE.RELEASE_NUM.eq("Working"), DT.STATE.in("WIP", "Draft", "Candidate"))))
+                .fetchInto(SummaryCc.class);
+
+        return unionOfSummaryCcList;
     }
 
     public List<SummaryCcExt> getSummaryCcExtList(BigInteger releaseId) {
