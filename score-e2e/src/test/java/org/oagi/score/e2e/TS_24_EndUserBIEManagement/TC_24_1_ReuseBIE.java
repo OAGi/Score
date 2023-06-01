8660b2ca550b474e42647b37fd02bc80553b078b
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-06-01 15:55:30 -0400

    update TC_24_1_1

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_24_EndUserBIEManagement/TC_24_1_ReuseBIE.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_24_EndUserBIEManagement/TC_24_1_ReuseBIE.java
@@ -132,7 +132,7 @@ public class TC_24_1_ReuseBIE extends BaseTest {
         SelectProfileBIEToReuseDialog  selectProfileBIEToReuseDialog =editBIEPage.reuseBIEOnNode("/" + asccp_owner_usera.getPropertyTerm() + "/Extension/" + asccp_reuse.getPropertyTerm());
         selectProfileBIEToReuseDialog.selectBIEToReuse(useraBIE);
 
-        assertEquals(1, getDriver().findElements(By.xpath("//span[.=\""+asccp.getPropertyTerm()+"\"]//ancestor::div/mat-icon[@role=\"img\"][@data-mat-icon-name=\"fa-recycle\"]")).size());
+        assertEquals(1, getDriver().findElements(By.xpath("//span[.=\""+asccp_reuse.getPropertyTerm()+"\"]//ancestor::div[1]/fa-icon")).size());
         WebElement asccpNode = editBIEPage.getNodeByPath("/" + asccp_owner_usera.getPropertyTerm() + "/" + asccp.getPropertyTerm());
         EditBIEPage.ASBIEPanel asbiePanel = editBIEPage.getASBIEPanel(asccpNode);
         asbiePanel.setCardinalityMax(199);
