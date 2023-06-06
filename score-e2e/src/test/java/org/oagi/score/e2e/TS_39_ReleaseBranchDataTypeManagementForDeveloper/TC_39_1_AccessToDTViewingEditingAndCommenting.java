ece120924dabdb8420b1813ad6c91e14bce1aeef
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-06-06 13:45:03 -0400

    Implementation of the TC_39_1_TA_2 assertion

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_39_ReleaseBranchDataTypeManagementForDeveloper/TC_39_1_AccessToDTViewingEditingAndCommenting.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_39_ReleaseBranchDataTypeManagementForDeveloper/TC_39_1_AccessToDTViewingEditingAndCommenting.java
@@ -12,12 +12,16 @@ import org.oagi.score.e2e.obj.DTObject;
 import org.oagi.score.e2e.obj.NamespaceObject;
 import org.oagi.score.e2e.obj.ReleaseObject;
 import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.code_list.AddCommentDialog;
+import org.oagi.score.e2e.page.core_component.DTViewEditPage;
 import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.oagi.score.e2e.AssertionHelper.assertDisabled;
+import static org.oagi.score.e2e.impl.PageHelper.escape;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_39_1_AccessToDTViewingEditingAndCommenting extends BaseTest {
@@ -78,6 +82,43 @@ public class TC_39_1_AccessToDTViewingEditingAndCommenting extends BaseTest {
             viewEditCoreComponentPage.setDEN(dt.getDen());
             viewEditCoreComponentPage.hitSearchButton();
             assertDoesNotThrow(() -> viewEditCoreComponentPage.getTableRecordByValue(dt.getDen()));
+        }
+
+    }
+
+    @Test
+    @DisplayName("TC_39_1_TA_2")
+    public void test_TA_2() {
+        AppUserObject developerA;
+        ReleaseObject branch;
+        ArrayList<DTObject> dtForTesting = new ArrayList<>();
+        {
+            developerA = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+            thisAccountWillBeDeletedAfterTests(developerA);
+
+            AppUserObject endUserA = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+            thisAccountWillBeDeletedAfterTests(endUserA);
+
+            branch = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("10.8.4");
+            NamespaceObject namespaceEU = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(endUserA);
+
+            DTObject baseDT = getAPIFactory().getCoreComponentAPI().getCDTByDENAndReleaseNum("Numeric. Type", branch.getReleaseNumber());
+
+            DTObject dtEUWIP = getAPIFactory().getCoreComponentAPI().createRandomBDT(baseDT, endUserA, namespaceEU, "WIP");
+            dtForTesting.add(dtEUWIP);
+        }
+
+        HomePage homePage = loginPage().signIn(developerA.getLoginId(), developerA.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage = homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        for (DTObject dt : dtForTesting) {
+            assertFalse(dt.getOwnerUserId().equals(developerA.getAppUserId()));
+            assertTrue(dt.getState().equals("WIP"));
+            DTViewEditPage dtViewEditPage = viewEditCoreComponentPage.openDTViewEditPageByDenAndBranch(dt.getDen(), branch.getReleaseNumber());
+            assertDisabled(dtViewEditPage.getDefinitionField());
+            assertDisabled(dtViewEditPage.getQualifierField());
+            AddCommentDialog addCommentDialog = dtViewEditPage.hitAddCommentButton("/" + dt.getDen());
+            addCommentDialog.setComment("test comment");
+            escape(getDriver());
         }
 
     }
