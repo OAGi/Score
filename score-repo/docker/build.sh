ee69cb577da7ba42b3b4e0a21740ff8194a26478
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-06-07 13:38:07 -0400

    update formatCheck for external csv upload

--- score-repo/docker/build.sh
+++ score-repo/docker/build.sh
@@ -6,9 +6,9 @@ if ! [[ -x "$(command -v docker)" ]]; then
 fi
 
 echo "Building docker image..."
-docker build --no-cache -f Dockerfile -t oagi1docker/srt-repo:3.0.0 .
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-repo:3.0.2 .
 
 echo "Scanning vulnerabilities..."
-docker scout cves oagi1docker/srt-repo:3.0.0
+docker scout cves oagi1docker/srt-repo:3.0.2
 
 echo "Done."
