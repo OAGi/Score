730c6bc3fd189c34a16ed1959b2db4b720c12f53
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-06-08 15:27:25 -0400

    Implementation of the TC_41_2 assertions

--- score-e2e/src/test/java/org/oagi/score/e2e/suite/TS_41.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/suite/TS_41.java
@@ -3,12 +3,13 @@ package org.oagi.score.e2e.suite;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
-import org.oagi.score.e2e.TS_41_ReleaseBranchDataTypeManagementForEndUser.TC_41_1_DTAccess;
+import org.oagi.score.e2e.TS_41_ReleaseBranchDataTypeManagementForEndUser.*;
 
 @Suite
 @SuiteDisplayName("Test Suite 41")
 @SelectClasses({
-        TC_41_1_DTAccess.class
+        TC_41_1_DTAccess.class,
+        TC_41_2_CreatingBrandNewDT.class
 })
 public class TS_41 {
 }
