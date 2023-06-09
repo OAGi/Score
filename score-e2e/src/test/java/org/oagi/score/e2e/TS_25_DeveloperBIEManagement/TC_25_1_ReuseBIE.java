69ece95924f7c4b726b1d81b89dfccb5055feb60
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-06-09 16:09:56 -0400

    add TC_25_1_ReuseBIE

---
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_25_DeveloperBIEManagement/TC_25_1_ReuseBIE.java
@@ -0,0 +1,4 @@
+package org.oagi.score.e2e.TS_25_DeveloperBIEManagement;
+
+public class TC_25_1_ReuseBIE {
+}
---
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_25_DeveloperBIEManagement/TC_25_2_CreateTopLevelBIEFromBIENode.java
@@ -0,0 +1,4 @@
+package org.oagi.score.e2e.TS_25_DeveloperBIEManagement;
+
+public @interface TC_25_2_CreateTopLevelBIEFromBIENode {
+}
