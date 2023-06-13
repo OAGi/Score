0553484aec2809fdd160908b635a2e9bb4e9add3
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-06-13 11:21:49 -0400

    add bie-express.openapi30 module

--- score-web/src/app/bie-management/bie-express/bie-express.module.ts
+++ score-web/src/app/bie-management/bie-express/bie-express.module.ts
@@ -12,6 +12,7 @@ import {MetaHeaderDialogComponent} from './meta-header-dialog/meta-header-dialog
 import {PaginationResponseDialogComponent} from './pagination-response-dialog/pagination-response-dialog.component';
 import {ScoreCommonModule} from '../../common/score-common.module';
 import { BieExpressOpenapi30Component } from './bie-express.openapi30/bie-express.openapi30.component';
+import {BieExpressOpenapi30Module} from './bie-express.openapi30/bie-express.openapi30.module';
 
 
 const routes: Routes = [
@@ -31,12 +32,12 @@ const routes: Routes = [
     TranslateModule,
     CommonModule,
     ScoreCommonModule,
+    BieExpressOpenapi30Module,
   ],
   declarations: [
     BieExpressComponent,
     MetaHeaderDialogComponent,
     PaginationResponseDialogComponent,
-    BieExpressOpenapi30Component,
   ],
   entryComponents: [
     MetaHeaderDialogComponent,
---
+++ score-web/src/app/bie-management/bie-express/bie-express.openapi30/bie-express.openapi30.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {BieExpressOpenapi30Component} from './bie-express.openapi30.component';
+
+
+
+@NgModule({
+  declarations: [BieExpressOpenapi30Component],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BieExpressOpenapi30Component
+  ]
+})
+export class BieExpressOpenapi30Module { }
