25c500a576bd6a84693001e393196af0f0005a96
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-06-14 16:02:52 -0400

    add openAPI-doc data model

--- score-web/src/app/bie-management/bie-express/bie-express.openapi30/domain/openapi-doc.ts
+++ score-web/src/app/bie-management/bie-express/bie-express.openapi30/domain/openapi-doc.ts
@@ -3,8 +3,9 @@ import {ParamMap} from '@angular/router';
 import {HttpParams} from '@angular/common/http';
 import {base64Decode, base64Encode} from '../../../../common/utility';
 import {ScoreUser} from '../../../../authentication/domain/auth';
+import {BusinessContext} from '../../../../context-management/business-context/domain/business-context';
 
-export class OpenAPIDocListRequest {
+export class OasDocListRequest {
   filters: {
     title: string;
     openAPIVersion: string;
@@ -93,7 +94,7 @@ export class OpenAPIDocListRequest {
     return (str) ? 'q=' + str : undefined;
   }
 }
-export class OpenAPIDoc{
+export class OasDoc{
   oasDocId: number;
   guid: string;
   openAPIVersion: string;
@@ -110,6 +111,95 @@ export class OpenAPIDoc{
   creationTimestamp: Date;
   createdBy: ScoreUser;
   lastUpdatedBy: ScoreUser;
+}
+
+export class OasDocBIEListRequest {
+  updaterUsernameList: string[] = [];
+  updatedDate: {
+    start: Date,
+    end: Date,
+  };
+  page: PageRequest = new PageRequest();
+
+  constructor(paramMap?: ParamMap, defaultPageRequest?: PageRequest) {
+    const q = (paramMap) ? paramMap.get('q') : undefined;
+    const params = (q) ? new HttpParams({fromString: base64Decode(q)}) : new HttpParams();
+
+    this.page.sortActive = params.get('sortActive');
+    if (!this.page.sortActive) {
+      this.page.sortActive = (defaultPageRequest) ? defaultPageRequest.sortActive : '';
+    }
+    this.page.sortDirection = params.get('sortDirection');
+    if (!this.page.sortDirection) {
+      this.page.sortDirection = (defaultPageRequest) ? defaultPageRequest.sortDirection : '';
+    }
+    if (params.get('pageIndex')) {
+      this.page.pageIndex = Number(params.get('pageIndex'));
+    } else {
+      this.page.pageIndex = (defaultPageRequest) ? defaultPageRequest.pageIndex : 0;
+    }
+    if (params.get('pageSize')) {
+      this.page.pageSize = Number(params.get('pageSize'));
+    } else {
+      this.page.pageSize = (defaultPageRequest) ? defaultPageRequest.pageSize : 0;
+    }
+
+    this.updaterUsernameList = (params.get('updaterUsernameList')) ? Array.from(params.get('updaterUsernameList').split(',')) : [];
+    this.updatedDate = {
+      start: (params.get('updatedDateStart')) ? new Date(params.get('updatedDateStart')) : null,
+      end: (params.get('updatedDateEnd')) ? new Date(params.get('updatedDateEnd')) : null
+    };
+  }
+
+  toParams(): HttpParams {
+    let params = new HttpParams()
+      .set('sortActive', this.page.sortActive)
+      .set('sortDirection', this.page.sortDirection)
+      .set('pageIndex', '' + this.page.pageIndex)
+      .set('pageSize', '' + this.page.pageSize);
+
+    if (this.updaterUsernameList && this.updaterUsernameList.length > 0) {
+      params = params.set('updaterUsernameList', this.updaterUsernameList.join(','));
+    }
+    if (this.updatedDate.start) {
+      params = params.set('updateStart', '' + this.updatedDate.start.getTime());
+    }
+    if (this.updatedDate.end) {
+      params = params.set('updateEnd', '' + this.updatedDate.end.getTime());
+    }
+    return params;
+  }
+
+  toQuery(): string {
+    const params = this.toParams();
+    const str = base64Encode(params.toString());
+    return (str) ? 'q=' + str : undefined;
+  }
+}
+
+export class OasDocBIEList{
+  topLevelAsbiepId: number;
+  den: string;
+  propertyTerm: string;
+  guid: string;
+  releaseNum: string;
+  bizCtxId: number;
+  bizCtxName: string;
+  access: string;
+  owner: string;
+  version: string;
+  status: string;
+  state: string;
+  businessContexts: BusinessContext[];
+  lastUpdateTimestamp: Date;
+  lastUpdateUser: string;
+  verb: string;
+  arrayIndicator: boolean;
+  suppressRoot: boolean;
+  messageBody: string;
+  resourceName: string;
+  operationId: string;
+  tagName: string;
 }
 
 
