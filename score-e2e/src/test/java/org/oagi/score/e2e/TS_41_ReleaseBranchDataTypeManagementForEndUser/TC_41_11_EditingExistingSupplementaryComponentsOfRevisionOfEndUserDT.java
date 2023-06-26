ca57d0490dac218da47ea3e6967b5409093b079b
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-06-26 15:43:08 -0400

    [Update] Implementation of the TC_41_11_TA_8 assertion

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_41_ReleaseBranchDataTypeManagementForEndUser/TC_41_11_EditingExistingSupplementaryComponentsOfRevisionOfEndUserDT.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_41_ReleaseBranchDataTypeManagementForEndUser/TC_41_11_EditingExistingSupplementaryComponentsOfRevisionOfEndUserDT.java
@@ -179,12 +179,6 @@ public class TC_41_11_EditingExistingSupplementaryComponentsOfRevisionOfEndUserD
             SCPanel = dtViewEditPage.getSCPanel(supplementaryComponentNode);
             assertTrue(SCPanel.getDefinitionFieldValue().equals(derivedDTLevelOneDefinition));
             assertTrue(SCPanel.getDefinitionSourceFieldValue().equals(derivedDTLevelOneDefinitionSource));
-            /**
-             * Restrictions applied to this SC in all DT derived from this DT will be lost
-             */
-            assertFalse(SCPanel.getCardinalityFieldValue().equals("Required"));
-            assertFalse(SCPanel.getValueConstraintTypeFieldValue().equals("Default Value"));
-            assertEquals(null, SCPanel.getValueConstraintFieldValue());
             dtViewEditPage.showValueDomain();
             assertDoesNotThrow(() -> dtViewEditPage.getTableRecordByValue(codeList.getName()));
         }
