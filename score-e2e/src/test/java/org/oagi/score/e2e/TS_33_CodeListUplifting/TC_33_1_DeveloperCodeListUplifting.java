3896dfa44e7c2263ae5d8249e24ba396617f8717
elenajelisic <71283987+elena-jelisic@users.noreply.github.com>
2023-07-05 23:02:46 -0400

    Implementation of the TC_33_1_TA_1 assertion

---
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_33_CodeListUplifting/TC_33_1_DeveloperCodeListUplifting.java
@@ -0,0 +1,60 @@
+package org.oagi.score.e2e.TS_33_CodeListUplifting;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.oagi.score.e2e.BaseTest;
+import org.oagi.score.e2e.obj.AppUserObject;
+import org.oagi.score.e2e.obj.DTObject;
+import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriverException;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.oagi.score.e2e.impl.PageHelper.click;
+
+@Execution(ExecutionMode.CONCURRENT)
+public class TC_33_1_DeveloperCodeListUplifting extends BaseTest {
+
+    private final List<AppUserObject> randomAccounts = new ArrayList<>();
+
+    @BeforeEach
+    public void init() {
+        super.init();
+
+    }
+
+    private void thisAccountWillBeDeletedAfterTests(AppUserObject appUser) {
+        this.randomAccounts.add(appUser);
+    }
+
+    @Test
+    @DisplayName("TC_33_1_TA_1")
+    public void test_TA_1() {
+        AppUserObject developer;
+
+        {
+            developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+            thisAccountWillBeDeletedAfterTests(developer);
+        }
+        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        assertThrows(WebDriverException.class, () -> homePage.getBIEMenu().getUpliftCodeListSubMenu(true));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        super.tearDown();
+        // Delete random accounts
+        this.randomAccounts.forEach(newUser -> {
+            getAPIFactory().getAppUserAPI().deleteAppUserByLoginId(newUser.getLoginId());
+        });
+    }
+}
