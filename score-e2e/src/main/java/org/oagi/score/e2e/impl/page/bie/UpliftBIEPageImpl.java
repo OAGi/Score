02c5e14edd87f8d08cd9f1e9fa688acb46b32a34
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-07-17 11:47:16 -0400

    update test_TA_29_1_1

--- score-e2e/src/main/java/org/oagi/score/e2e/impl/page/bie/UpliftBIEPageImpl.java
+++ score-e2e/src/main/java/org/oagi/score/e2e/impl/page/bie/UpliftBIEPageImpl.java
@@ -6,10 +6,46 @@ import org.oagi.score.e2e.page.bie.UpliftBIEPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import static org.oagi.score.e2e.impl.PageHelper.getText;
-import static org.oagi.score.e2e.impl.PageHelper.visibilityOfElementLocated;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.oagi.score.e2e.impl.PageHelper.*;
 
 public class UpliftBIEPageImpl extends BasePageImpl implements UpliftBIEPage {
+    private static final By SOURCE_BRANCH_SELECT_FIELD_LOCATOR =
+            By.xpath("//*[contains(text(), \"Source Branch\")]//ancestor::mat-form-field[1]//mat-select//div[contains(@class, \"mat-select-arrow-wrapper\")]");
+
+    private static final By TARGET_BRANCH_SELECT_FIELD_LOCATOR =
+            By.xpath("//*[contains(text(), \"Target Branch\")]//ancestor::mat-form-field[1]//mat-select//div[contains(@class, \"mat-select-arrow-wrapper\")]");
+
+    private static final By STATE_SELECT_FIELD_LOCATOR =
+            By.xpath("//*[contains(text(), \"State\")]//ancestor::mat-form-field[1]//mat-select//div[contains(@class, \"mat-select-arrow-wrapper\")]");
+
+    private static final By OWNER_SELECT_FIELD_LOCATOR =
+            By.xpath("//mat-label[contains(text(), \"Owner\")]//ancestor::div[1]/mat-select[1]");
+
+    private static final By UPDATER_SELECT_FIELD_LOCATOR =
+            By.xpath("//*[contains(text(), \"Updater\")]//ancestor::div[1]/mat-select[1]");
+
+    private static final By PROPERTY_TERM_FIELD_LOCATOR =
+            By.xpath("//span[contains(text(), \"Property Term\")]//ancestor::mat-form-field//input");
+
+    private static final By BUSINESS_CONTEXT_FIELD_LOCATOR =
+            By.xpath("//span[contains(text(), \"Business Context\")]//ancestor::mat-form-field//input");
+
+    private static final By DROPDOWN_SEARCH_FIELD_LOCATOR =
+            By.xpath("//input[@aria-label=\"dropdown search\"]");
+
+    private static final By UPDATED_START_DATE_FIELD_LOCATOR =
+            By.xpath("//input[contains(@data-placeholder, \"Updated start date\")]");
+
+    private static final By UPDATED_END_DATE_FIELD_LOCATOR =
+            By.xpath("//input[contains(@data-placeholder, \"Updated end date\")]");
+
+    private static final By SEARCH_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Search\")]//ancestor::button[1]");
+    private static final By NEXT_BUTTON_LOCATOR =
+            By.xpath("//span[contains(text(), \"Next\")]//ancestor::button[1]");
 
     public UpliftBIEPageImpl(BasePage parent) {
         super(parent);
@@ -30,5 +66,151 @@ public class UpliftBIEPageImpl extends BasePageImpl implements UpliftBIEPage {
     @Override
     public WebElement getTitle() {
         return visibilityOfElementLocated(getDriver(), By.className("mat-card-title"));
+    }
+
+    @Override
+    public WebElement getSourceBranchSelectField() {
+        return visibilityOfElementLocated(getDriver(), SOURCE_BRANCH_SELECT_FIELD_LOCATOR);
+    }
+    @Override
+    public void setSourceBranch(String sourceBranch) {
+        click(getSourceBranchSelectField());
+        WebElement searchedSelectField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[text() = \"" + sourceBranch + "\"]"));
+        click(searchedSelectField);
+        escape(getDriver());
+    }
+
+    @Override
+    public WebElement getTargetBranchSelectField() {
+        return visibilityOfElementLocated(getDriver(), TARGET_BRANCH_SELECT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setTargetBranch(String targetBranch) {
+        click(getTargetBranchSelectField());
+        WebElement searchedSelectField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[text() = \"" + targetBranch + "\"]"));
+        click(searchedSelectField);
+        escape(getDriver());
+    }
+
+    @Override
+    public WebElement getStateSelectField() {
+        return visibilityOfElementLocated(getDriver(), STATE_SELECT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setState(String state) {
+        click(getStateSelectField());
+        WebElement optionField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[contains(text(), \"" + state + "\")]"));
+        click(optionField);
+        escape(getDriver());
+    }
+
+    @Override
+    public WebElement getOwnerSelectField() {
+        return visibilityOfElementLocated(getDriver(), OWNER_SELECT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setOwner(String owner) {
+        click(getOwnerSelectField());
+        sendKeys(visibilityOfElementLocated(getDriver(), DROPDOWN_SEARCH_FIELD_LOCATOR), owner);
+        WebElement searchedSelectField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[contains(text(), \"" + owner + "\")]"));
+        click(searchedSelectField);
+        escape(getDriver());
+
+    }
+
+    @Override
+    public WebElement getUpdaterSelectField() {
+        return visibilityOfElementLocated(getDriver(), UPDATER_SELECT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdater(String updater) {
+        click(getUpdaterSelectField());
+        sendKeys(visibilityOfElementLocated(getDriver(), DROPDOWN_SEARCH_FIELD_LOCATOR), updater);
+        WebElement searchedSelectField = visibilityOfElementLocated(getDriver(),
+                By.xpath("//mat-option//span[contains(text(), \"" + updater + "\")]"));
+        click(searchedSelectField);
+        escape(getDriver());
+
+    }
+
+    @Override
+    public WebElement getPropertyTermField() {
+        return visibilityOfElementLocated(getDriver(), PROPERTY_TERM_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setPropertyTerm(String propertyTerm) {
+        sendKeys(getPropertyTermField(), propertyTerm);
+    }
+
+    @Override
+    public WebElement getBusinessContextField() {
+        return visibilityOfElementLocated(getDriver(), BUSINESS_CONTEXT_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setBusinessContext(String businessContext) {
+        sendKeys(getBusinessContextField(), businessContext);
+    }
+
+    @Override
+    public WebElement getUpdatedStartDateField() {
+        return visibilityOfElementLocated(getDriver(), UPDATED_START_DATE_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdatedStartDate(LocalDateTime updatedStartDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        sendKeys(getUpdatedStartDateField(), formatter.format(updatedStartDate));
+    }
+
+    @Override
+    public WebElement getUpdatedEndDateField() {
+        return visibilityOfElementLocated(getDriver(), UPDATED_END_DATE_FIELD_LOCATOR);
+    }
+
+    @Override
+    public void setUpdatedEndDate(LocalDateTime updatedEndDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        sendKeys(getUpdatedEndDateField(), formatter.format(updatedEndDate));
+    }
+
+    @Override
+    public WebElement getSearchButton() {
+        return elementToBeClickable(getDriver(), SEARCH_BUTTON_LOCATOR);
+    }
+
+    @Override
+    public void hitSearchButton() {
+        click(getSearchButton());
+        invisibilityOfLoadingContainerElement(getDriver());
+    }
+
+    @Override
+    public WebElement getTableRecordAtIndex(int idx) {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//tbody/tr[" + idx + "]"));
+    }
+
+    @Override
+    public WebElement getTableRecordByValue(String value) {
+        return visibilityOfElementLocated(getDriver(), By.xpath("//td//span[contains(text(), \"" + value + "\")]/ancestor::tr"));
+    }
+
+    @Override
+    public WebElement getColumnByName(WebElement tableRecord, String columnName) {
+        return tableRecord.findElement(By.className("mat-column-" + columnName));
+    }
+
+    @Override
+    public WebElement getNextButton() {
+        return elementToBeClickable(getDriver(), NEXT_BUTTON_LOCATOR);
     }
 }
--- score-e2e/src/test/java/org/oagi/score/e2e/TS_29_BIEUplifting/TC_29_1_BIEUplifting.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_29_BIEUplifting/TC_29_1_BIEUplifting.java
@@ -6,10 +6,27 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.oagi.score.e2e.BaseTest;
-import org.oagi.score.e2e.obj.AppUserObject;
+import org.oagi.score.e2e.api.CoreComponentAPI;
+import org.oagi.score.e2e.menu.BIEMenu;
+import org.oagi.score.e2e.obj.*;
+import org.oagi.score.e2e.page.HomePage;
+import org.oagi.score.e2e.page.bie.*;
+import org.oagi.score.e2e.page.core_component.ACCExtensionViewEditPage;
+import org.oagi.score.e2e.page.core_component.SelectAssociationDialog;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.oagi.score.e2e.impl.PageHelper.escape;
+import static org.oagi.score.e2e.impl.PageHelper.getText;
+
 @Execution(ExecutionMode.CONCURRENT)
 public class TC_29_1_BIEUplifting extends BaseTest {
     private List<AppUserObject> randomAccounts = new ArrayList<>();
@@ -35,10 +52,116 @@ public class TC_29_1_BIEUplifting extends BaseTest {
 
     @Test
     public void test_TA_29_1_1() {
-
+        AppUserObject usera;
+        String prev_release = "10.8.6";
+        String curr_release = "10.8.8";
+        usera = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+        thisAccountWillBeDeletedAfterTests(usera);
+        HomePage homePage = loginPage().signIn(usera.getLoginId(), usera.getPassword());
+        BIEMenu bieMenu = homePage.getBIEMenu();
+        UpliftBIEPage upliftBIEPage = bieMenu.openUpliftBIESubMenu();
+        upliftBIEPage.setSourceBranch(curr_release);
+        assertThrows(TimeoutException.class, () -> upliftBIEPage.setTargetBranch(prev_release));
     }
+
     @Test
     public void test_TA_29_1_2() {
+        ASCCPObject asccp, asccp_owner_usera, asccp_to_append, asccp_child, asccp_reuse;
+        BCCPObject bccp, bccp_to_append, bccp_child, bccp_not_reuse;
+        ACCObject acc;
+        AppUserObject usera;
+        NamespaceObject namespace;
+        BusinessContextObject context;
+        TopLevelASBIEPObject useraBIE;
+        String prev_release = "10.8.6";
+        String curr_release = "10.8.8";
+        ReleaseObject prevReleaseObject = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber(prev_release);
+        ReleaseObject currReleaseObject = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber(curr_release);
+        {
+            usera = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+            thisAccountWillBeDeletedAfterTests(usera);
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+            namespace = getAPIFactory().getNamespaceAPI().createRandomEndUserNamespace(usera);
+
+            /**
+             * The owner of the ASCCP is usera
+             */
+            acc = coreComponentAPI.createRandomACC(usera, prevReleaseObject, namespace, "Production");
+            DTObject dataType = coreComponentAPI.getBDTByGuidAndReleaseNum("dd0c8f86b160428da3a82d2866a5b48d", prevReleaseObject.getReleaseNumber());
+            bccp = coreComponentAPI.createRandomBCCP(dataType, usera, namespace, "Production");
+            bccp_child = coreComponentAPI.createRandomBCCP(dataType, usera, namespace, "Production");
+            coreComponentAPI.appendBCC(acc, bccp, "Production");
+
+            DTObject dataTypeWithSC = coreComponentAPI.getBDTByGuidAndReleaseNum("3292eaa5630b48ecb7c4249b0ddc760e", prevReleaseObject.getReleaseNumber());
+            bccp_not_reuse = coreComponentAPI.createRandomBCCP(dataTypeWithSC, usera, namespace, "Production");
+
+            ACCObject acc_association = coreComponentAPI.createRandomACC(usera, prevReleaseObject, namespace, "Production");
+            ACCObject acc_association2 = coreComponentAPI.createRandomACC(usera, prevReleaseObject, namespace, "Production");
+
+            bccp_to_append = coreComponentAPI.createRandomBCCP(dataType, usera, namespace, "Production");
+            coreComponentAPI.appendBCC(acc_association, bccp_to_append, "Production");
+
+            asccp_child = coreComponentAPI.createRandomASCCP(acc_association, usera, namespace, "Production");
+            ASCCObject ascc = coreComponentAPI.appendASCC(acc_association2, asccp_child, "Production");
+            ascc.setCardinalityMax(1);
+            coreComponentAPI.updateASCC(ascc);
+
+            asccp = coreComponentAPI.createRandomASCCP(acc_association, usera, namespace, "Production");
+            asccp_to_append = coreComponentAPI.createRandomASCCP(acc_association, usera, namespace, "Production");
+            asccp_reuse = coreComponentAPI.createRandomASCCP(acc_association2, usera, namespace, "Production");
+            ascc = coreComponentAPI.appendASCC(acc, asccp, "Production");
+            coreComponentAPI.appendExtension(acc, usera, namespace, "Published");
+            asccp_owner_usera = coreComponentAPI.createRandomASCCP(acc, usera, namespace, "Production");
+
+            context = getAPIFactory().getBusinessContextAPI().createRandomBusinessContext(usera);
+            useraBIE = getAPIFactory().getBusinessInformationEntityAPI().generateRandomTopLevelASBIEP(Collections.singletonList(context), asccp_reuse, usera, "WIP");
+        }
+
+        HomePage homePage = loginPage().signIn(usera.getLoginId(), usera.getPassword());
+        BIEMenu bieMenu = homePage.getBIEMenu();
+        ViewEditBIEPage viewEditBIEPage = bieMenu.openViewEditBIESubMenu();
+        CreateBIEForSelectTopLevelConceptPage createBIEForSelectTopLevelConceptPage = viewEditBIEPage.openCreateBIEPage().next(Collections.singletonList(context));
+        createBIEForSelectTopLevelConceptPage.createBIE(asccp_owner_usera.getDen(), prev_release);
+        viewEditBIEPage.openPage();
+        viewEditBIEPage.setDEN(asccp_owner_usera.getDen());
+        viewEditBIEPage.hitSearchButton();
+        WebElement tr = viewEditBIEPage.getTableRecordAtIndex(1);
+        EditBIEPage editBIEPage = viewEditBIEPage.openEditBIEPage(tr);
+        ACCExtensionViewEditPage ACCExtensionViewEditPage = editBIEPage.extendBIELocallyOnNode("/" + asccp_owner_usera.getPropertyTerm() + "/Extension");
+        SelectAssociationDialog selectCCPropertyPage = ACCExtensionViewEditPage.appendPropertyAtLast("/" + asccp_owner_usera.getPropertyTerm() + " User Extension Group. Details");
+        selectCCPropertyPage.selectAssociation(asccp_to_append.getDen());
+        selectCCPropertyPage = ACCExtensionViewEditPage.appendPropertyAtLast("/" + asccp_owner_usera.getPropertyTerm() + " User Extension Group. Details");
+        selectCCPropertyPage.selectAssociation(bccp_not_reuse.getDen());
+        selectCCPropertyPage = ACCExtensionViewEditPage.appendPropertyAtLast("/" + asccp_owner_usera.getPropertyTerm() + " User Extension Group. Details");
+        selectCCPropertyPage.selectAssociation(asccp_reuse.getDen());
+        ACCExtensionViewEditPage.setNamespace(namespace);
+        ACCExtensionViewEditPage.hitUpdateButton();
+        ACCExtensionViewEditPage.moveToQA();
+        ACCExtensionViewEditPage.moveToProduction();
+        editBIEPage.openPage();
+        editBIEPage.clickOnDropDownMenuByPath("/" + asccp_owner_usera.getPropertyTerm() + "/Extension/" + bccp_not_reuse.getPropertyTerm());
+        assertEquals(0, getDriver().findElements(By.xpath("//span[contains(text(),\"Reuse BIE\")]")).size());
+
+        escape(getDriver());
+        editBIEPage.openPage();
+        SelectProfileBIEToReuseDialog selectProfileBIEToReuseDialog = editBIEPage.reuseBIEOnNode("/" + asccp_owner_usera.getPropertyTerm() + "/Extension/" + asccp_reuse.getPropertyTerm());
+        selectProfileBIEToReuseDialog.selectBIEToReuse(useraBIE);
+
+        assertEquals(0, getDriver().findElements(By.xpath("//span[.=\"" + asccp_reuse.getPropertyTerm() + "\"]//ancestor::div[1]/fa-icon")).size());
+        WebElement asccpNode = editBIEPage.getNodeByPath("/" + asccp_owner_usera.getPropertyTerm() + "/" + asccp.getPropertyTerm());
+        EditBIEPage.ASBIEPanel asbiePanel = editBIEPage.getASBIEPanel(asccpNode);
+        asbiePanel.toggleUsed();
+        asbiePanel.setCardinalityMax(199);
+        asbiePanel.setCardinalityMin(77);
+        asbiePanel.setContextDefinition("aContextDefinition");
+        editBIEPage.hitUpdateButton();
+
+        editBIEPage.openPage();
+        asccpNode = editBIEPage.getNodeByPath("/" + asccp_owner_usera.getPropertyTerm() + "/" + asccp.getPropertyTerm());
+        asbiePanel = editBIEPage.getASBIEPanel(asccpNode);
+        assertEquals("199", getText(asbiePanel.getCardinalityMaxField()));
+        assertEquals("77", getText(asbiePanel.getCardinalityMinField()));
+        assertEquals("aContextDefinition", getText(asbiePanel.getContextDefinitionField()));
 
     }
 
