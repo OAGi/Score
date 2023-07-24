081ccb7e8230f2fdde7942e2599bb7a5fc4d9334
Bingqi Cheng <bingqi.cheng@nist.gov>
2023-07-24 12:34:48 -0400

    update test_TA_19_1_3a

--- score-e2e/src/test/java/org/oagi/score/e2e/TS_19_ReleaseManagement/TC_19_1_ReleaseManagement.java
+++ score-e2e/src/test/java/org/oagi/score/e2e/TS_19_ReleaseManagement/TC_19_1_ReleaseManagement.java
@@ -14,9 +14,7 @@ import org.oagi.score.e2e.obj.*;
 import org.oagi.score.e2e.page.HomePage;
 import org.oagi.score.e2e.page.code_list.EditCodeListPage;
 import org.oagi.score.e2e.page.code_list.ViewEditCodeListPage;
-import org.oagi.score.e2e.page.core_component.ACCViewEditPage;
-import org.oagi.score.e2e.page.core_component.SelectAssociationDialog;
-import org.oagi.score.e2e.page.core_component.ViewEditCoreComponentPage;
+import org.oagi.score.e2e.page.core_component.*;
 import org.oagi.score.e2e.page.namespace.CreateNamespacePage;
 import org.oagi.score.e2e.page.namespace.ViewEditNamespacePage;
 import org.oagi.score.e2e.page.release.CreateReleasePage;
@@ -28,10 +26,12 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 
+import java.math.BigInteger;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -39,16 +39,22 @@ import static org.oagi.score.e2e.AssertionHelper.assertDisabled;
 import static org.oagi.score.e2e.impl.PageHelper.*;
 import static org.oagi.score.e2e.impl.PageHelper.elementToBeClickable;
 
-@Execution(ExecutionMode.CONCURRENT)
+@Execution(ExecutionMode.SAME_THREAD)
 public class TC_19_1_ReleaseManagement extends BaseTest {
     private List<AppUserObject> randomAccounts = new ArrayList<>();
-    AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
+    AppUserObject devx = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
     AppUserObject endUser = getAPIFactory().getAppUserAPI().createRandomEndUserAccount(false);
+    NamespaceObject developerNamespace = getAPIFactory().getNamespaceAPI().createRandomDeveloperNamespace(devx);
     String existingReleaseNum = null;
     String newReleaseNum = String.valueOf((RandomUtils.nextInt(20230716, 20231231)));
     CodeListObject codeListCandidate;
     RandomCoreComponentWithStateContainer developerCoreComponentWithStateContainer;
     RandomCoreComponentWithStateContainer euCoreComponentWithStateContainer;
+
+    Map<String, ACCObject> testingACCs = new HashMap<>();
+    Map<String, ASCCPObject> testingASCCPs = new HashMap<>();
+    Map<String, BCCPObject> testingBCCPs = new HashMap<>();
+
 
     @BeforeEach
     public void init() {
@@ -69,9 +75,9 @@ public class TC_19_1_ReleaseManagement extends BaseTest {
         ccStates.add("Draft");
         ccStates.add("Candidate");
         ccStates.add("Deleted");
-        developerCoreComponentWithStateContainer = new RandomCoreComponentWithStateContainer(developer, workingBranch, namespace, ccStates);
+        developerCoreComponentWithStateContainer = new RandomCoreComponentWithStateContainer(devx, workingBranch, namespace, ccStates);
         ACCObject candidateACC = developerCoreComponentWithStateContainer.stateACCs.get("Candidate");
-        HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+        HomePage homePage = loginPage().signIn(devx.getLoginId(), devx.getPassword());
         ViewEditCoreComponentPage viewEditCoreComponentPage = homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
         ACCViewEditPage accViewEditPage = viewEditCoreComponentPage.openACCViewEditPageByManifestID(candidateACC.getAccManifestId());
         accViewEditPage.backToWIP();
@@ -81,8 +87,8 @@ public class TC_19_1_ReleaseManagement extends BaseTest {
         accViewEditPage.moveToCandidate();
 
         codeListCandidate = getAPIFactory().getCodeListAPI().
-                createRandomCodeList(developer, namespace, workingBranch, "Published");
-        getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeListCandidate, developer);
+                createRandomCodeList(devx, namespace, workingBranch, "Published");
+        getAPIFactory().getCodeListValueAPI().createRandomCodeListValue(codeListCandidate, devx);
 
         ViewEditCodeListPage viewEditCodeListPage = homePage.getCoreComponentMenu().openViewEditCodeListSubMenu();
         EditCodeListPage editCodeListPage = viewEditCodeListPage.openCodeListViewEditPageByNameAndBranch(codeListCandidate.getName(), "Working");
@@ -133,7 +139,7 @@ public class TC_19_1_ReleaseManagement extends BaseTest {
         if (existingReleaseNum != null) {
             try {
                 // move the draft release back to initialized state
-                HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
+                HomePage homePage = loginPage().signIn(devx.getLoginId(), devx.getPassword());
                 ViewEditReleasePage viewEditReleasePage = homePage.getCoreComponentMenu().openViewEditReleaseSubMenu();
                 viewEditReleasePage.MoveBackToInitialized(existingReleaseNum);
                 waitFor(Duration.ofSeconds(60L));
@@ -200,8 +206,126 @@ public class TC_19_1_ReleaseManagement extends BaseTest {
         viewEditReleasePage.openPage();
         assertThrows(NoSuchElementException.class, () ->viewEditReleasePage.openReleaseViewEditPageByReleaseAndState(newReleaseNum, "Initialized"));
     }
+
     @Test
     public void test_TA_19_1_3a() {
+
+        String branch = "Working";
+        HomePage homePage = loginPage().signIn(devx.getLoginId(), devx.getPassword());
+        ViewEditCoreComponentPage viewEditCoreComponentPage =
+                homePage.getCoreComponentMenu().openViewEditCoreComponentSubMenu();
+        {
+            ReleaseObject workingRelease = getAPIFactory().getReleaseAPI().getReleaseByReleaseNumber("Working");
+            CoreComponentAPI coreComponentAPI = getAPIFactory().getCoreComponentAPI();
+
+            ACCObject ACCreleaseTA3devxwip = coreComponentAPI.createRandomACC(devx, workingRelease, developerNamespace, "WIP");
+            ACCreleaseTA3devxwip.setObjectClassTerm("ACCrelease TA3devxwip");
+            coreComponentAPI.updateACC(ACCreleaseTA3devxwip);
+            if (!testingACCs.containsKey("ACCreleaseTA3devxwip")) {
+                testingACCs.put("ACCreleaseTA3devxwip", ACCreleaseTA3devxwip);
+            } else {
+                testingACCs.put("ACCreleaseTA3devxwip", ACCreleaseTA3devxwip);
+            }
+
+            ACCObject ACCreleaseTA3devxdraft = coreComponentAPI.createRandomACC(devx, workingRelease, developerNamespace, "Draft");
+            ACCreleaseTA3devxdraft.setObjectClassTerm("ACCrelease TA3devxdraft");
+            coreComponentAPI.updateACC(ACCreleaseTA3devxdraft);
+            if (!testingACCs.containsKey("ACCreleaseTA3devxdraft")) {
+                testingACCs.put("ACCreleaseTA3devxdraft", ACCreleaseTA3devxdraft);
+            } else {
+                testingACCs.put("ACCreleaseTA3devxdraft", ACCreleaseTA3devxdraft);
+            }
+
+            ACCObject ACCreleaseTA3devxcandidate = coreComponentAPI.createRandomACC(devx, workingRelease, developerNamespace, "Candidate");
+            ACCreleaseTA3devxcandidate.setObjectClassTerm("ACCrelease TA3devxcandidate");
+            coreComponentAPI.updateACC(ACCreleaseTA3devxcandidate);
+            if (!testingACCs.containsKey("ACCreleaseTA3devxcandidate")) {
+                testingACCs.put("ACCreleaseTA3devxcandidate", ACCreleaseTA3devxcandidate);
+            } else {
+                testingACCs.put("ACCreleaseTA3devxcandidate", ACCreleaseTA3devxcandidate);
+            }
+
+            ASCCPCreateDialog asccpCreateDialog = viewEditCoreComponentPage.openASCCPCreateDialog(branch);
+            ASCCPViewEditPage asccpViewEditPage = asccpCreateDialog.create("Entity Identifiers Group. Details");
+            String url = getDriver().getCurrentUrl();
+            BigInteger asccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            ASCCPObject ASCCPreleaseTA3devxwip = getAPIFactory().getCoreComponentAPI().getASCCPByManifestId(asccpManifestId);
+            if (!testingASCCPs.containsKey("ASCCPreleaseTA3devxwip")) {
+                testingASCCPs.put("ASCCPreleaseTA3devxwip", ASCCPreleaseTA3devxwip);
+            } else {
+                testingASCCPs.put("ASCCPreleaseTA3devxwip",ASCCPreleaseTA3devxwip);
+            }
+
+            viewEditCoreComponentPage.openPage();
+            asccpCreateDialog = viewEditCoreComponentPage.openASCCPCreateDialog(branch);
+            asccpViewEditPage = asccpCreateDialog.create("Entity Identifiers Group. Details");
+            url = getDriver().getCurrentUrl();
+            asccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            ASCCPObject ASCCPreleaseTA3devxdraft = getAPIFactory().getCoreComponentAPI().getASCCPByManifestId(asccpManifestId);
+            asccpViewEditPage.moveToDraft();
+            if (!testingASCCPs.containsKey("ASCCPreleaseTA3devxdraft")) {
+                testingASCCPs.put("ASCCPreleaseTA3devxdraft", ASCCPreleaseTA3devxdraft);
+            } else {
+                testingASCCPs.put("ASCCPreleaseTA3devxdraft",ASCCPreleaseTA3devxdraft);
+            }
+
+            viewEditCoreComponentPage.openPage();
+            asccpCreateDialog = viewEditCoreComponentPage.openASCCPCreateDialog(branch);
+            asccpViewEditPage = asccpCreateDialog.create("Entity Identifiers Group. Details");
+            url = getDriver().getCurrentUrl();
+            asccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            ASCCPObject ASCCPreleaseTA3devxcandidate = getAPIFactory().getCoreComponentAPI().getASCCPByManifestId(asccpManifestId);
+            asccpViewEditPage.moveToDraft();
+            asccpViewEditPage.moveToCandidate();
+            if (!testingASCCPs.containsKey("ASCCPreleaseTA3devxcandidate")) {
+                testingASCCPs.put("ASCCPreleaseTA3devxcandidate", ASCCPreleaseTA3devxcandidate);
+            } else {
+                testingASCCPs.put("ASCCPreleaseTA3devxcandidate",ASCCPreleaseTA3devxcandidate);
+            }
+
+            viewEditCoreComponentPage.openPage();
+            BCCPCreateDialog bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+            BCCPViewEditPage bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+            url = getDriver().getCurrentUrl();
+            BigInteger bccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            BCCPObject BCCPreleaseTA3devxwip = getAPIFactory().getCoreComponentAPI().getBCCPByManifestId(bccpManifestId);
+
+            if (!testingBCCPs.containsKey("BCCPreleaseTA3devxwip")) {
+                testingBCCPs.put("BCCPreleaseTA3devxwip", BCCPreleaseTA3devxwip);
+            } else {
+                testingBCCPs.put("BCCPreleaseTA3devxwip",BCCPreleaseTA3devxwip);
+            }
+
+            viewEditCoreComponentPage.openPage();
+            bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+            bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+            url = getDriver().getCurrentUrl();
+            bccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            BCCPObject BCCPreleaseTA3devxdraft = getAPIFactory().getCoreComponentAPI().getBCCPByManifestId(bccpManifestId);
+            bccpViewEditPage.moveToDraft();
+
+            if (!testingBCCPs.containsKey("BCCPreleaseTA3devxdraft")) {
+                testingBCCPs.put("BCCPreleaseTA3devxdraft", BCCPreleaseTA3devxdraft);
+            } else {
+                testingBCCPs.put("BCCPreleaseTA3devxdraft",BCCPreleaseTA3devxdraft);
+            }
+
+            viewEditCoreComponentPage.openPage();
+            bccpCreateDialog = viewEditCoreComponentPage.openBCCPCreateDialog(branch);
+            bccpViewEditPage = bccpCreateDialog.create("System Environment_ Code. Type");
+            url = getDriver().getCurrentUrl();
+            bccpManifestId = new BigInteger(url.substring(url.lastIndexOf("/") + 1));
+            BCCPObject BCCPreleaseTA3devxcandidate = getAPIFactory().getCoreComponentAPI().getBCCPByManifestId(bccpManifestId);
+            bccpViewEditPage.moveToDraft();
+            bccpViewEditPage.moveToCandidate();
+
+            if (!testingBCCPs.containsKey("BCCPreleaseTA3devxcandidate")) {
+                testingBCCPs.put("BCCPreleaseTA3devxcandidate", BCCPreleaseTA3devxcandidate);
+            } else {
+                testingBCCPs.put("BCCPreleaseTA3devxcandidate",BCCPreleaseTA3devxcandidate);
+            }
+
+        }
         AppUserObject developer = getAPIFactory().getAppUserAPI().createRandomDeveloperAccount(false);
         thisAccountWillBeDeletedAfterTests(developer);
         HomePage homePage = loginPage().signIn(developer.getLoginId(), developer.getPassword());
@@ -210,9 +334,10 @@ public class TC_19_1_ReleaseManagement extends BaseTest {
         assertEquals(existingReleaseNum, getText(editReleasePage.getReleaseNumberField()));
     }
 
-
     @Test
     public void test_TA_19_1_3b() {
+
+
 
 
 
