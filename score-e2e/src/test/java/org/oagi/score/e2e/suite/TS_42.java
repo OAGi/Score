7c6e2b2b5008e87bc1b112f0cb1dd077854e0e2a
Hakju Oh <hakju.oh@gmail.com>
2023-08-04 10:50:37 -0400

    Update TS_42

---
+++ score-e2e/src/test/java/org/oagi/score/e2e/suite/TS_42.java
@@ -0,0 +1,20 @@
+package org.oagi.score.e2e.suite;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.oagi.score.e2e.TS_42_BusinessTerm.TC_42_1_EndUserViewOrEditBusinessTerm;
+import org.oagi.score.e2e.TS_42_BusinessTerm.TC_42_2_BusinessTermAssignment;
+import org.oagi.score.e2e.TS_42_BusinessTerm.TC_42_3_BusinessTermFromBIEDetailPage;
+import org.oagi.score.e2e.TS_42_BusinessTerm.TC_42_4_LoadBusinessTermsFromExternalSource;
+
+@Suite
+@SuiteDisplayName("Test Suite 42")
+@SelectClasses({
+        TC_42_1_EndUserViewOrEditBusinessTerm.class,
+        TC_42_2_BusinessTermAssignment.class,
+        TC_42_3_BusinessTermFromBIEDetailPage.class,
+        TC_42_4_LoadBusinessTermsFromExternalSource.class
+})
+public class TS_42 {
+}
