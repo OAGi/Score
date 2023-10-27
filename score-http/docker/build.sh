90dd3cebd634af824d32e2d83e00cd29dc226001
Hakju Oh <hakju.oh@gmail.com>
2023-10-27 13:05:54 -0400

    Update version to 3.1.2

--- score-http/docker/build.sh
+++ score-http/docker/build.sh
@@ -10,14 +10,14 @@ cd ..
 ./mvnw clean package -DskipTests=true
 
 echo "Preparing files..."
-cp score-http/target/score-http-3.1.1.war docker
+cp score-http/target/score-http-3.1.2.war docker
 cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.2.0/mariadb-java-client-3.2.0.jar docker
 
 echo "Building docker image..."
 cd docker
-docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.1.1 .
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.1.2 .
 
 echo "Scanning vulnerabilities..."
-docker scout cves oagi1docker/srt-http-gateway:3.1.1
+docker scout cves oagi1docker/srt-http-gateway:3.1.2
 
 echo "Done."
