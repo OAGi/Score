2688aea65967e7a7c9a63965e31d0c1adbd69050
Hakju Oh <hakju.oh@gmail.com>
2024-01-23 14:27:12 -0500

    Update srt-repo docker image to 3.2.1

--- score-repo/docker/build.sh
+++ score-repo/docker/build.sh
@@ -6,9 +6,9 @@ if ! [[ -x "$(command -v docker)" ]]; then
 fi
 
 echo "Building docker image..."
-docker build --no-cache -f Dockerfile -t oagi1docker/srt-repo:3.2.0 .
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-repo:3.2.1 .
 
 echo "Scanning vulnerabilities..."
-docker scout cves oagi1docker/srt-repo:3.2.0
+docker scout cves oagi1docker/srt-repo:3.2.1
 
 echo "Done."
