2f414417b0eaf4a885aadc976374814b02d12c19
Hakju Oh <hakju.oh@gmail.com>
2024-02-08 16:08:52 -0500

    Add redis sentinel / cluster settings

--- score-http/score-http/src/main/java/org/oagi/score/redis/configuration/RedisConfiguration.java
+++ score-http/score-http/src/main/java/org/oagi/score/redis/configuration/RedisConfiguration.java
@@ -7,13 +7,22 @@ import org.redisson.config.TransportMode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableTransactionManagement
@@ -25,11 +34,44 @@ public class RedisConfiguration {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.sentinel.master}")
+    private String redisSentinelMaster;
+
+    @Value("${spring.data.redis.sentinel.nodes}")
+    private String redisSentinelNodes;
+
+    @Value("${spring.data.redis.cluster.nodes}")
+    private String redisClusterNodes;
+
+    private Set<String> getRedisSentinelNodes() {
+        if (StringUtils.hasLength(redisSentinelNodes)) {
+            return Arrays.asList(redisSentinelNodes.split(",")).stream()
+                    .map(e -> e.trim()).filter(e -> StringUtils.hasLength(e)).collect(Collectors.toSet());
+        }
+        return Collections.emptySet();
+    }
+
+    private List<String> getRedisClusterNodes() {
+        if (StringUtils.hasLength(redisClusterNodes)) {
+            return Arrays.asList(redisClusterNodes.split(",")).stream()
+                    .map(e -> e.trim()).filter(e -> StringUtils.hasLength(e)).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(
-                new RedisStandaloneConfiguration(redisHost, redisPort)
-        );
+        org.springframework.data.redis.connection.RedisConfiguration redisConfiguration;
+        List<String> redisClusterNodes = getRedisClusterNodes();
+        if (!redisClusterNodes.isEmpty()) {
+            redisConfiguration = new RedisClusterConfiguration(redisClusterNodes);
+        } else if (StringUtils.hasLength(redisSentinelMaster)) {
+            Set<String> redisSentinelNodes = getRedisSentinelNodes();
+            redisConfiguration = new RedisSentinelConfiguration(redisSentinelMaster, redisSentinelNodes);
+        } else {
+            redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        }
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisConfiguration);
         return connectionFactory;
     }
 
@@ -37,8 +79,29 @@ public class RedisConfiguration {
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.setTransportMode(TransportMode.NIO);
-        config.useSingleServer()
-                .setAddress("redis://" + redisHost + ":" + redisPort);
+        List<String> redisClusterNodes = getRedisClusterNodes();
+        if (!redisClusterNodes.isEmpty()) {
+            config.useClusterServers()
+                    .setNodeAddresses(redisClusterNodes.stream().map(e -> {
+                        if (e.startsWith("redis://") || e.startsWith("rediss://")) {
+                            return e;
+                        }
+                        return "redis://" + e;
+                    }).collect(Collectors.toList()));
+        } else if (StringUtils.hasLength(redisSentinelMaster)) {
+            Set<String> redisSentinelNodes = getRedisSentinelNodes();
+            config.useSentinelServers()
+                    .setMasterName(redisSentinelMaster)
+                    .setSentinelAddresses(redisSentinelNodes.stream().map(e -> {
+                        if (e.startsWith("redis://") || e.startsWith("rediss://")) {
+                            return e;
+                        }
+                        return "redis://" + e;
+                    }).collect(Collectors.toList()));
+        } else {
+            config.useSingleServer()
+                    .setAddress("redis://" + redisHost + ":" + redisPort);
+        }
         return Redisson.create(config);
     }
 
