9583d6a7dc5f54f8442c023f1d665c9d3c8a9eee
Hakju Oh <hakju.oh@gmail.com>
2024-03-06 10:12:55 -0500

    Update dependencies to latest versions

--- score-http/docker/build.sh
+++ score-http/docker/build.sh
@@ -11,7 +11,7 @@ cd ..
 
 echo "Preparing files..."
 cp score-http/target/score-http-3.2.2.war docker
-cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.3.2/mariadb-java-client-3.3.2.jar docker
+cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.3.3/mariadb-java-client-3.3.3.jar docker
 
 echo "Building docker image..."
 cd docker
@@ -19,5 +19,8 @@ docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.2.2 .
 
 echo "Scanning vulnerabilities..."
 docker scout cves oagi1docker/srt-http-gateway:3.2.2
+
+echo "Cleaning up..."
+rm -f *.jar *.war
 
 echo "Done."
