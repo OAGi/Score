7ac0d3c345b970e99fdac7d79bda97f9ad8466d4
Hakju Oh <hakju.oh@gmail.com>
2024-06-26 10:57:30 -0400

    Update conf.py

--- docs/user_guide/conf.py
+++ docs/user_guide/conf.py
@@ -24,9 +24,9 @@ copyright = u'2024, NIST'
 author = u'NIST'
 
 # The short X.Y version
-version = u'3.3.0'
+version = u'3.3.1'
 # The full version, including alpha/beta/rc tags
-release = u'3.3.0'
+release = u'3.3.1'
 
 
 # -- General configuration ---------------------------------------------------
