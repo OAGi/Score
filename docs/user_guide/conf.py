9fd247fd574a810cd434b7cb05960420cb5a7a4e
Hakju Oh <hakju.oh@gmail.com>
2024-10-03 10:13:20 -0400

    Update version to 3.3.3

--- docs/user_guide/conf.py
+++ docs/user_guide/conf.py
@@ -24,9 +24,9 @@ copyright = u'2024, NIST'
 author = u'NIST'
 
 # The short X.Y version
-version = u'3.3.2'
+version = u'3.3.3'
 # The full version, including alpha/beta/rc tags
-release = u'3.3.2'
+release = u'3.3.3'
 
 
 # -- General configuration ---------------------------------------------------
