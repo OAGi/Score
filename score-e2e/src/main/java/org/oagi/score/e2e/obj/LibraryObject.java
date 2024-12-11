bedac1aeff5547734021995b84be9efa2ad6e944
Hakju Oh <hakju.oh@gmail.com>
2024-12-11 14:07:08 -0500

    Update/Fix test scripts

---
+++ score-e2e/src/main/java/org/oagi/score/e2e/obj/LibraryObject.java
@@ -0,0 +1,14 @@
+package org.oagi.score.e2e.obj;
+
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Data
+public class LibraryObject {
+
+    private BigInteger libraryId;
+
+    private String libraryName;
+
+}
