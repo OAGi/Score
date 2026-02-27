0e27b418af4ad3c72f84622165e35e083987bd42
Hakju Oh <hakju.oh@gmail.com>
2026-02-27 09:37:43 -0500

    Update version to 3.4.3-rc.1

--- score-http/docker/build.sh
+++ score-http/docker/build.sh
@@ -13,15 +13,15 @@ cd ..
 ./mvnw clean package -DskipTests=true
 
 echo "Preparing files..."
-cp target/score-http-3.4.2.war docker
+cp target/score-http-3.4.3-rc.1.war docker
 cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.5.7/mariadb-java-client-3.5.7.jar docker
 
 echo "Building docker image..."
 cd docker
-docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.4.2 .
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.4.3-rc.1 .
 
 echo "Scanning vulnerabilities..."
-docker scout cves oagi1docker/srt-http-gateway:3.4.2
+docker scout cves oagi1docker/srt-http-gateway:3.4.3-rc.1
 
 echo "Cleaning up..."
 rm -f *.jar *.war
